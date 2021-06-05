577898f9735e53567ae6b5267c571686c3e2838b
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-06-05 09:03:28 +0200

    kernel: Improved coding style in the idt code.

--- sources/arch/idt.c
+++ sources/arch/idt.c
@@ -1,6 +1,7 @@
-#include "arch/idt.h"
 #include <library/io.h>
+
 #include "arch.h"
+#include "arch/idt.h"
 
 extern uintptr_t __interrupt_vector[128];
 
@@ -8,7 +9,7 @@ static struct idt idt = {};
 
 static struct idt_descriptor idt_descriptor = {
     .size = sizeof(struct idt) - 1,
-    .offset = (uintptr_t)&idt
+    .offset = (uintptr_t)&idt,
 };
 
 struct idt_entry idt_entry(uintptr_t handler, uint8_t ist, uint8_t idt_flags)
@@ -16,11 +17,11 @@ struct idt_entry idt_entry(uintptr_t handler, uint8_t ist, uint8_t idt_flags)
     return (struct idt_entry){
         .attributes = idt_flags,
         .ist = ist,
-        
+
         .offset_0_16 = (handler),
         .offset_16_32 = (handler >> 16),
         .offset_32_63 = (handler >> 32),
-        
+
         .code_segment = 0x8,
         ._zero = 0,
     };
@@ -32,17 +33,20 @@ void idt_initialize(void)
     {
         idt.entries[i] = idt_entry(__interrupt_vector[i], 0, IDT_GATE);
     }
- 
-    // not used for the moment
-    //idt[127] = idt_entry((void *)__interrupt_vector[48], 0, IDT_GATE | IDT_USER); // syscall interrupt
-    //idt[100] = idt_entry((void *)__interrupt_vector[49], 1, IDT_GATE | IDT_USER); // multi processor signal interrupt 
+
+    // FIXME: Not used for the moment
+
+    // syscall interrupt
+    //idt[127] = idt_entry((void *)__interrupt_vector[48], 0, IDT_GATE | IDT_USER);
+
+    // multi processor signal interrupt
+    //idt[100] = idt_entry((void *)__interrupt_vector[49], 1, IDT_GATE | IDT_USER);
 
     idt_update(&idt_descriptor);
-    
 }
 
-void* interrupt_handler(struct interrupt_stackframe* stackframe)
+void *interrupt_handler(struct interrupt_stackframe *stackframe)
 {
-    print(arch_log(), "interrupt {} !\n", (long)(stackframe->int_no)); 
+    print(arch_log(), "interrupt {} !\n", (long)(stackframe->int_no));
     return stackframe;
 }
