8b762885ad5740f09a715a20faa6a52d093ed5a1
https://supercip971@github.com <supercyp@protonmail.com>
2021-06-05 09:51:23 -0400

    libds: add a basic bitmap data structure

---
+++ sources/library/ds/bitmap.c
@@ -0,0 +1,22 @@
+#include "ds/bitmap.h"
+#include "mem.h"
+
+struct bitmap bitmap(void* data, size_t size)
+{
+    return (struct bitmap){
+        .data = data,
+        .size = size,
+    };
+}
+
+void bitmap_fill(struct bitmap* bitmap, bool value)
+{
+    if(value)
+    {
+        mem_set(bitmap->data, 0xff, bitmap->size/8);
+    }
+    else
+    {
+        mem_set(bitmap->data, 0   , bitmap->size/8);
+    }
+}
