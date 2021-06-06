a88499085d03bb710bf1d669a5c23e9868c258ab
https://supercip971@github.com <supercyp@protonmail.com>
2021-06-06 11:40:56 -0400

    kernel: move som vmm struct to paging.h

--- sources/arch/x86_64/paging.h
+++ sources/arch/x86_64/paging.h
@@ -2,119 +2,49 @@
 
 #include <library/base/macros.h>
 #include <library/base/std.h>
-
-struct PACKED pml4_entry
+#include <library/mem.h>
+struct PACKED pml
 {
-    bool present : 1;               // Must be 1 to reference a PML-1
-    bool writable : 1;              // If 0, writes may not be allowed.
-    bool user : 1;                  // If 0, user-mode accesses are not allowed
-    bool write_thought : 1;         // Page-level write-through
-    bool cache : 1;                 // Page-level cache disable
-    bool accessed : 1;              // Indicates whether this entry has been used
-    int zero0 : 6;                  // Ignored
-    uint64_t physical_address : 36; // Physical address of a 4-KByte aligned PLM-1
-    int zero1 : 15;                 // Ignored
-    bool execute_disabled : 1;      // If IA32_EFER.NXE = 1, Execute-disable
+    union
+    {
+        uint64_t _raw;
+
+        struct PACKED
+        {
+            bool present : 1;
+            bool read_write : 1;
+            bool user : 1;
+            bool caching : 1;
+            bool caching_disable : 1;
+            bool accessed : 1;
+            bool dirty : 1;
+            bool huge_page : 1;
+            bool global_page : 1;
+            uint8_t _available : 3;
+            uint64_t physical : 52;
+        };
+    };
 };
-
-struct PACKED pml4
+// by default create a present page
+static inline struct pml page(uintptr_t physical, size_t flags)
 {
-    struct pml4_entry entries[512];
-};
-
-static inline size_t pml4_index(uintptr_t address)
-{
-    return (address >> 39) & 0x1FF;
+    return (struct pml){
+        .physical = physical >> 12,
+        .user = (flags & BR_MEM_USER) != 0,
+        .read_write = (flags & BR_MEM_WRITABLE) != 0,
+        .present = true,
+        .caching = false,
+        .caching_disable = false,
+        .accessed = false,
+        .dirty = false,
+        .huge_page = false,
+        .global_page = false,
+        ._available = 0};
 }
 
-static_assert(sizeof(struct pml4_entry) == sizeof(uint64_t), "");
-static_assert(sizeof(struct pml4) == 4096, "");
+static_assert(sizeof(struct pml) == sizeof(uint64_t), "pml must be 64 bit");
 
-struct PACKED pml3_entry
-{
-    bool present : 1;               // Must be 1 to reference a PML-1
-    bool writable : 1;              // If 0, writes may not be allowed.
-    bool user : 1;                  // If 0, user-mode accesses are not allowed
-    bool write_thought : 1;         // Page-level write-through
-    bool cache : 1;                 // Page-level cache disable
-    bool accessed : 1;              // Indicates whether this entry has been used
-    int zero0 : 1;                  // Ignored
-    int size : 1;                   // Must be 0 otherwise, this entry maps a 1-GByte page.
-    int zero1 : 4;                  // Ignored
-    uint64_t physical_address : 36; // Physical address of a 4-KByte aligned PLM-1
-    int zero2 : 15;                 // Ignored
-    bool execute_disabled : 1;      // If IA32_EFER.NXE = 1, Execute-disable
-};
-
-struct PACKED pml3
-{
-    struct pml3_entry entries[512];
-};
-
-static inline size_t pml3_index(uintptr_t address)
-{
-    return (address >> 30) & 0x1FF;
-}
-
-static_assert(sizeof(struct pml3_entry) == sizeof(uint64_t), "");
-static_assert(sizeof(struct pml3) == 4096, "");
-
-struct PACKED pml2_entry
-{
-    bool present : 1;               // Must be 1 to reference a PML-1
-    bool writable : 1;              // If 0, writes may not be allowed.
-    bool user : 1;                  // If 0, user-mode accesses are not allowed
-    bool write_thought : 1;         // Page-level write-through
-    bool cache : 1;                 // Page-level cache disable
-    bool accessed : 1;              // Indicates whether this entry has been used
-    int zero0 : 1;                  // Ignored
-    int size : 1;                   // Must be 0 otherwise, this entry maps a 2-MByte page.
-    int zero1 : 4;                  // Ignored
-    uint64_t physical_address : 36; // Physical address of a 4-KByte aligned PLM-1
-    int zero2 : 15;                 // Ignored
-    bool execute_disabled : 1;      // If IA32_EFER.NXE = 1, Execute-disable
-};
-
-struct PACKED pml2
-{
-    struct pml3_entry entries[512];
-};
-
-static inline size_t pml2_index(uintptr_t address)
-{
-    return (address >> 21) & 0x1FF;
-}
-
-static_assert(sizeof(struct pml2_entry) == sizeof(uint64_t), "");
-static_assert(sizeof(struct pml2) == 4096, "");
-
-struct PACKED pml1_entry
-{
-    bool present : 1;               // Must be 1 to reference a PML-1
-    bool writable : 1;              // If 0, writes may not be allowed.
-    bool user : 1;                  // If 0, user-mode accesses are not allowed
-    bool write_thought : 1;         // Page-level write-through
-    bool cache : 1;                 // Page-level cache disable
-    bool accessed : 1;              // Indicates whether this entry has been used
-    int dirty : 1;                  // Indicates whether software has accessed the 4-KByte page referenced by this entry
-    int memory_type : 1;            // Indirectly determines the memory type used to access the 4-KByte page referenced by this entry.
-    int global : 1;                 // If CR4.PGE = 1, determines whether the translation is global.
-    int zero0 : 3;                  // Ignored
-    uint64_t physical_address : 36; // Physical address of a 4-KByte aligned PLM-1
-    int zero1 : 10;                 // Ignored
-    int protection_key : 5;         // If CR4.PKE = 1, determines the protection key of the page.
-    bool execute_disabled : 1;      // If IA32_EFER.NXE = 1, Execute-disable
-};
-
-struct PACKED pml1
-{
-    struct pml3_entry entries[512];
-};
-
-static inline size_t pml1_index(uintptr_t address)
-{
-    return (address >> 12) & 0x1FF;
-}
-
-static_assert(sizeof(struct pml1_entry) == sizeof(uint64_t), "");
-static_assert(sizeof(struct pml1) == 4096, "");
+#define PML4_GET_INDEX(addr) (((uint64_t)addr & ((uint64_t)0x1ff << 39)) >> 39)
+#define PDPT_GET_INDEX(addr) (((uint64_t)addr & ((uint64_t)0x1ff << 30)) >> 30)
+#define PAGE_DIR_GET_INDEX(addr) (((uint64_t)addr & ((uint64_t)0x1ff << 21)) >> 21)
+#define PAGE_TABLE_GET_INDEX(addr) (((uint64_t)addr & ((uint64_t)0x1ff << 12)) >> 12)
