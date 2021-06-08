2908c5b20da049bd12c6dd7760a1fdf07bc8151e
https://supercip971@github.com <supercyp@protonmail.com>
2021-06-08 14:53:29 -0400

    libio: added lock for logger

--- sources/host/kernel/log.c
+++ sources/host/kernel/log.c
@@ -1,9 +1,13 @@
 #include <host/log.h>
+#include <library/task/lock.h>
 
 #include "arch/arch.h"
 
+struct lock logger_lock;
+
 void host_log_lock(void)
 {
+    lock_acquire(&logger_lock);
 }
 
 struct writer *host_log_writer(void)
@@ -13,4 +17,5 @@ struct writer *host_log_writer(void)
 
 void host_log_unlock(void)
 {
+    lock_release(&logger_lock);
 }
