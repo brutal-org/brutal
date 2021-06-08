756cae16cf1096a75e98abbead63dc8aa2f27d1a
https://supercip971@github.com <supercyp@protonmail.com>
2021-06-08 09:02:34 -0400

    kernel: added madt records tables

--- sources/arch/x86_64/acpi.c
+++ sources/arch/x86_64/acpi.c
@@ -40,7 +40,24 @@ struct acpi_madt_record *acpi_madt_record(
 
     return nullptr;
 }
+struct acpi_madt_record_table acpi_madt_multiple_record(struct acpi_madt *madt, enum acpi_madt_record_type type)
+{
+    struct acpi_madt_record_table result = {};
+    struct acpi_madt_record *current = madt->records;
 
+    while ((uintptr_t)current < (uintptr_t)&madt->base + madt->base.length)
+    {
+        if (current->type == type)
+        {
+            result.table[result.count] = current;
+            result.count++;
+        }
+
+        current = (struct acpi_madt_record *)(((uintptr_t)current) + current->lenght);
+    }
+
+    return result;
+}
 struct acpi_madt *acpi_find_madt(uintptr_t rsdp_address)
 {
     auto rsdp = mmap_io_ptr_from_phys(struct acpi_rsdp *, rsdp_address);
@@ -62,4 +79,34 @@ uint32_t acpi_find_ioapic(uintptr_t rsdp_address)
     auto ioapic = (struct acpi_madt_ioapic_record *)acpi_madt_record(madt, ACPI_MADT_RECORD_IOAPIC);
 
     return ioapic->address;
+}
+struct lapic_record_table acpi_find_lapic_table(uintptr_t rsdp_address)
+{
+    auto madt = acpi_find_madt(rsdp_address);
+    struct acpi_madt_record_table table = acpi_madt_multiple_record(madt, ACPI_MADT_RECORD_LAPIC);
+    struct lapic_record_table final =
+        {
+            .count = table.count,
+
+        };
+    for (size_t i = 0; i < table.count; i++)
+    {
+        final.table[i] = (struct acpi_madt_lapic_record *)table.table[i];
+    }
+    return final;
+}
+
+struct ioapic_record_table acpi_find_ioapic_table(uintptr_t rsdp_address)
+{
+    auto madt = acpi_find_madt(rsdp_address);
+    struct acpi_madt_record_table table = acpi_madt_multiple_record(madt, ACPI_MADT_RECORD_IOAPIC);
+    struct ioapic_record_table final =
+        {
+            .count = table.count,
+        };
+    for (size_t i = 0; i < table.count; i++)
+    {
+        final.table[i] = (struct acpi_madt_ioapic_record *)table.table[i];
+    }
+    return final;
 }
