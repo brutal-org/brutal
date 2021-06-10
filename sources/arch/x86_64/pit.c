495f3297f9e4e642d9029a0e8e7e20c1d30a7303
Keyboard Slayer <dalcjor@libmail.eu>
2021-06-10 18:50:19 +0200

    chore: remove pit's magic value

--- sources/arch/x86_64/pit.c
+++ sources/arch/x86_64/pit.c
@@ -1,10 +1,11 @@
 #include "arch/x86_64/asm.h"
+#include "pit.h"
 
-void pit_initialize(int frequency)
+void pit_initialize(int freq_divisor)
 {
-    uint16_t divisor = 1193182 / frequency;
+    uint16_t divisor = PIT_FREQUENCY / freq_divisor;
 
-    asm_out8(0x43, 0x36);
-    asm_out8(0x40, divisor & 0xFF);
-    asm_out8(0x40, (divisor >> 8) & 0xFF);
+    asm_out8(PIT_MODE_COMMAND, PIT_CHANNEL1 | PIT_LOWBYTE | PIT_SQUARE_WAVE);
+    asm_out8(PIT_DATA_PORT0, divisor & 0xFF);
+    asm_out8(PIT_DATA_PORT0, (divisor >> 8) & 0xFF);
 }
--- sources/arch/x86_64/pit.h
+++ sources/arch/x86_64/pit.h
@@ -1,3 +1,21 @@
 #pragma once
 
+#define PIT_FREQUENCY 1193182
+
+#include <stdint.h>
+
+enum pit_reg
+{
+    PIT_MODE_COMMAND = 0x43,
+    PIT_DATA_PORT0 = 0x40,
+};
+
+enum pit_usage 
+{
+    PIT_CHANNEL1 = 1 << 5,
+    PIT_LOWBYTE = 1 << 4,
+    PIT_SQUARE_WAVE = 6
+};
+
 void pit_initialize(int frequency);
+void pit_sleep(uint16_t ms);
