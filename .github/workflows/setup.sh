b6a4c7a7f6494a085ae19af89bf275f135c8321a
https://supercip971@github.com <supercyp@protonmail.com>
2021-06-13 11:34:15 -0400

    ci: add gcc 11 install

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -8,8 +8,13 @@ deb-src http://apt.llvm.org/groovy/ llvm-toolchain-groovy-12 main
 
 sudo apt update
 
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt install -y gcc-11 g++-11
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 100 --slave /usr/bin/g++ g++ /usr/bin/g++-11 --slave /usr/bin/gcov gcov /usr/bin/gcov-11
+
 git submodule update --init --recursive
 
 sudo apt install -y make build-essential bison flex texinfo nasm mtools wget \
                     unzip fuse libfuse-dev uuid-dev gcc binutils parted \
                     clang-12 llvm-12
+                    
