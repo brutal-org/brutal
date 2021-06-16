d172f7b9f7456c43a18c3f815d5535a54d02b4f5
https://supercip971@github.com <supercyp@protonmail.com>
2021-06-16 16:59:17 -0400

    libc: add strcpy/cmp/ncmp

--- sources/libc/ansi/string.c
+++ sources/libc/ansi/string.c
@@ -13,7 +13,18 @@ void *memcpy(void *restrict s1, void const *restrict s2, size_t n)
 
 // void *memmove(void *s1,  void const *s2, size_t n) {}
 
-// char *strcpy(char *restrict s1,  char const *restrict s2) {}
+char *strcpy(char *restrict s1, char const *restrict s2)
+{
+    size_t i = 0;
+
+    while (s2[i] != 0)
+    {
+        s1[i] = s2[i];
+        i++;
+    }
+
+    return s1;
+}
 
 // char *strncpy(char *restrict s1,  char const *restrict s2, size_t n) {}
 
@@ -27,11 +38,35 @@ void *memcpy(void *restrict s1, void const *restrict s2, size_t n)
 
 // int memcmp(void const *s1, const void *s2, size_t n) {}
 
-// int strcmp(char const*s1, char const*s2) {}
+int strcmp(char const *s1, char const *s2)
+{
+    while (*s1 == *s2 && *s1)
+    {
+        s1++;
+        s2++;
+    };
+
+    return (int)*s1 - (int)*s2;
+}
 
 // int strcoll(char const *s1, char const*s2) {}
 
-// int strncmp(char const *s1, char const *s2, size_t n) {}
+int strncmp(char const *s1, char const *s2, size_t n)
+{
+    while (*s1 == *s2 && *s1 && n)
+    {
+        s1++;
+        s2++;
+        n--;
+    }
+
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    return (int)*s1 - (int)*s2;
+}
 
 // size_t strxfrm(char *restrict s1, char const *restrict s2, size_t n) {}
 
@@ -50,7 +85,7 @@ char *strrchr(char const *s, int c)
     size_t length = strlen(s);
     size_t pos = length;
 
-    while (s[pos] != c && pos-- > 0)
+    while (s[pos] != c && pos-- != 0)
         ;
 
     if (pos == length)
