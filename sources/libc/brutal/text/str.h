224b9426bb806b2dc6403e577938f2dfec45a150
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-06-20 23:37:33 +0200

    library: Improved str.h structure.

--- sources/libc/brutal/text/str.h
+++ sources/libc/brutal/text/str.h
@@ -4,18 +4,35 @@
 #include <brutal/base/std.h>
 #include <brutal/mem.h>
 
+/* --- Null Terminated String ----------------------------------------------- */
+
+static inline size_t cstr_len(char const *str)
+{
+    size_t size = 0;
+
+    while (str[size])
+    {
+        size++;
+    }
+
+    return size;
+}
+
+/* --- Non Owning Strings --------------------------------------------------- */
+
 typedef struct
 {
     char *buffer;
     size_t len;
 } str_t;
 
-typedef struct
-{
-    char *buffer;
-    size_t len;
-    size_t capacity;
-} str_buf_t;
+#define nullstr str_cast("")
+
+void str_rvs(str_t str);
+
+bool str_eq(str_t lhs, str_t rhs);
+
+/* --- Fix Size Strings ----------------------------------------------------- */
 
 #define str_fix_t(N)    \
     struct              \
@@ -30,19 +47,7 @@ typedef str_fix_t(32) str_fix32_t;
 typedef str_fix_t(64) str_fix64_t;
 typedef str_fix_t(128) str_fix128_t;
 
-#define nullstr str_cast("")
-
-static inline size_t cstr_len(char const *str)
-{
-    size_t size = 0;
-
-    while (str[size])
-    {
-        size++;
-    }
-
-    return size;
-}
+/* --- Cast Between String Types -------------------------------------------- */
 
 static inline str_t str_forward(str_t str) { return str; }
 static inline str_t str_make_from_cstr(char const *cstr) { return (str_t){(char *)cstr, cstr_len(cstr)}; }
@@ -54,32 +59,30 @@ static inline str_t str_make_from_str_fix128(str_fix128_t *str_fix) { return (st
 
 // clang-format off
 
-#define str_cast(literal)                \
-    _Generic((literal),                  \
-        str_t: str_forward,              \
-        char*: str_make_from_cstr,       \
-        char const*: str_make_from_cstr,  \
-        str_fix8_t*: str_make_from_str_fix8, \
-        str_fix16_t*: str_make_from_str_fix16, \
-        str_fix32_t*: str_make_from_str_fix32, \
-        str_fix64_t*: str_make_from_str_fix64, \
-        str_fix128_t*: str_make_from_str_fix128 \
+// Create a new instance of a non owning string.
+#define str_cast(literal)                         \
+    _Generic((literal),                           \
+        str_t         : str_forward,              \
+        char*         : str_make_from_cstr,       \
+        char const*   : str_make_from_cstr,       \
+        str_fix8_t*   : str_make_from_str_fix8,   \
+        str_fix16_t*  : str_make_from_str_fix16,  \
+        str_fix32_t*  : str_make_from_str_fix32,  \
+        str_fix64_t*  : str_make_from_str_fix64,  \
+        str_fix128_t* : str_make_from_str_fix128  \
     )(literal)
 
-#define str_cast_fix(T, str)                                  \
-    ({                                                        \
-        T dst_str = {};                                            \
+// clang-format on
+
+#define str_cast_n(str, n) \
+    (str_t) { str, n }
+
+// Create a new instance of a fix size string.
+#define str_cast_fix(T, str) (                                \
+    {                                                         \
+        T dst_str = {};                                       \
         auto src_str = str_cast(str);                         \
         mem_cpy(dst_str.buffer, src_str.buffer, src_str.len); \
         dst_str.len = src_str.len;                            \
         dst_str;                                              \
     })
-
-#define str_cast_n(str, n) \
-    (str_t){str, n}
-
-// clang-format on
-
-void str_rvs(str_t str);
-
-bool str_eq(str_t lhs, str_t rhs);
