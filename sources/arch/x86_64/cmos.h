9a16f2a6fa02b389c36991eef12fcc56688de9ce
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-06-27 17:50:42 +0200

    arch-x86_64: Implement cmos rtc.

---
+++ sources/arch/x86_64/cmos.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <brutal/base.h>
+#include <brutal/time.h>
+
+#define CMOS_ADDRESS 0x70
+#define CMOS_DATA 0x71
+
+enum cmos_regs
+{
+    CMOS_RTC_SECOND = 0x00,
+    CMOS_RTC_MINUTE = 0x02,
+    CMOS_RTC_HOUR = 0x04,
+    CMOS_RTC_DAY = 0x07,
+    CMOS_RTC_MONTH = 0x08,
+    CMOS_RTC_YEAR = 0x09,
+
+    CMOS_STATUS_A = 0x0A,
+};
+
+uint8_t cmos_read(enum cmos_regs reg);
+
+br_datetime_t cmos_read_rtc(void);
