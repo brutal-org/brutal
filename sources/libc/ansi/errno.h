a926c0d8736692b49d6bbe7b1959dd0118eeee6d
Nekro <70316327+NeKroFR@users.noreply.github.com>
2021-07-02 17:15:01 +0200

    libc: Declare errno.h (#24)
    * Create errno.h
    
    * Update errno.h
    
    * Update errno.h
    
    * remove one ;
    
    * Update errno.h
    
    * Update errno.h
    
    * Update errno.h

---
+++ sources/libc/ansi/errno.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/* --- 7.5 Errors <errno.h> ----------------------------------------------- */
+
+#define EDOM (2)
+
+#define EILSEQ (3)
+
+#define ERANGE (4)
+
+extern int errno = (0);
