be7b84fe6455c0a3927ccb83f82f73dd94378c03
Nekro <70316327+NeKroFR@users.noreply.github.com>
2021-07-03 17:23:11 +0200

    Update assert.h

--- sources/libc/ansi/assert.h
+++ sources/libc/ansi/assert.h
@@ -1,16 +1,13 @@
 #pragma once
 
-/* ---define assert in terms of NDEBUG ----------------------------------------------- */ 
+/* ---7.2 Diagnostics <assert.h> ----------------------------------------------- */ 
+
+void assert_failled_impl();
 
 #ifdef NDEBUG
-#define assert(ignore) ((void)0)
+#  define assert(expr) (void)(expr)
 #else
-#define assert(cond) 
+#  define assert(expr) if (!expr) { assert_failled_impl(); } 
 #endif
-
-
-
-/* --- static assert ----------------------------------------------- */ 
-
 
 #define static_assert _Static_assert
