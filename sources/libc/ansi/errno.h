e6233a97b0f22f9f842f71e1a6604adf804990cd
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-07-04 09:25:47 +0200

    libc: Improved errno.

--- sources/libc/ansi/errno.h
+++ sources/libc/ansi/errno.h
@@ -1,6 +1,6 @@
 #pragma once
 
-/* --- 7.5 Errors <errno.h> ----------------------------------------------- */
+/* --- 7.5 Errors ----------------------------------------------------------- */
 
 #define EDOM (2)
 
@@ -9,3 +9,7 @@
 #define ERANGE (4)
 
 extern int errno = (0);
+
+#if __has_include_next(<errno.h>)
+#    include_next <errno.h>
+#endif
