e5c687a59df7f59ccbe677d643a6fdea0ae1791b
Nekro <70316327+NeKroFR@users.noreply.github.com>
2021-07-06 16:04:19 +0200

    libc: Declare assert.h (#23)
    * Create assert.h
    
    * This is valid C
    
    * make assert.h work
    
    Normalement cette fois c'est la bonne !
    
    * Patching fautes de frappes
    
    * Update assert.h
    
    * Update assert.h

---
+++ sources/libc/ansi/assert.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/* --- 7.2 Diagnostics <assert.h> ----------------------------------------------- */ 
+
+void assert_failled_impl();
+
+#ifdef NDEBUG
+#  define assert(expr) (void)(expr)
+#else
+#  define assert(expr) if (!expr) { assert_failled_impl(); } 
+#endif
+
+#define static_assert _Static_assert
