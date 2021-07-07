4833b8ab46f89357e1873dcf11f5d10baa2f62d4
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-07-07 09:50:05 +0200

    brutal-ds: Linear.h was missing from ds.h and also extend the interface.

--- sources/libc/brutal/ds.h
+++ sources/libc/brutal/ds.h
@@ -1,5 +1,6 @@
 #pragma once
 
 #include <brutal/ds/bitmap.h>
+#include <brutal/ds/linear.h>
 #include <brutal/ds/slot.h>
 #include <brutal/ds/vec.h>
--- sources/libc/brutal/ds/linear.h
+++ sources/libc/brutal/ds/linear.h
@@ -2,22 +2,62 @@
 
 #include <brutal/alloc/base.h>
 
-typedef struct LinearBufferImpl
+typedef struct
 {
-    void *head;
-    void *tail;
+    size_t size;
+    uint8_t data[];
+} LinearBufferNodeImpl;
+
+typedef struct
+{
+    LinearBufferNodeImpl *head;
+    LinearBufferNodeImpl *tail;
+
     size_t allocated;
     struct alloc *alloc;
-};
+} LinearBufferImpl;
 
-#define LinearBuffer(T)        \
-    union                      \
-    {                          \
-        struct                 \
-        {                      \
-            T *head;           \
-            T *tail;           \
-            size_t allocated;  \
-        };                     \
-        LinearBufferImpl impl; \
+#define LinearBufferNode(T)         \
+    union                           \
+    {                               \
+        struct                      \
+        {                           \
+            size_t size;            \
+            T data;                 \
+        };                          \
+        LinearBufferNodeImpl _impl; \
     }
+
+#define LinearBuffer(T)                 \
+    union                               \
+    {                                   \
+        struct                          \
+        {                               \
+            LinearBufferNode(T) * head; \
+            LinearBufferNode(T) * tail; \
+        };                              \
+        LinearBufferImpl _impl;         \
+    }
+
+void linear_buffer_init_impl(LinearBufferImpl *impl, struct alloc *alloc);
+
+void linear_buffer_deinit_impl(LinearBufferImpl *impl);
+
+void linear_buffer_push_impl(LinearBufferImpl *impl, void *data, size_t size);
+
+void linear_buffer_clear_impl(LinearBufferImpl *impl);
+
+#define linear_buffer_init(SELF, ALLOC) \
+    linear_buffer_init_impl(&(SELF)->_impl, (ALLOC))
+
+#define linear_buffer_deinit(SELF) \
+    linear_buffer_deinit_impl(&(SELF)->_impl)
+
+#define linear_buffer_push(SELF, VALUE) (                          \
+    {                                                              \
+        auto tmp = (VALUE);                                        \
+        linear_buffer_push_impl(&(SELF)->_impl, &tmp, sizeof(tmp)) \
+    })
+
+#define linear_buffer_clear(SELF) \
+    linear_buffer_clear_impl(&(SELF)->_impl)
