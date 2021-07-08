614d8d5a8f0826b5ae400a176b03885d1aa334b9
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-07-08 18:21:54 +0200

    brutal-alloc: Added malloc and free helpers.

--- sources/libc/brutal/alloc/base.h
+++ sources/libc/brutal/alloc/base.h
@@ -35,14 +35,6 @@ struct alloc
         ptr;                                     \
     })
 
-#define alloc_calloc(SELF, SIZE, COUNT) (              \
-    {                                                  \
-        void *ptr = alloc_acquire(SELF, SIZE * COUNT); \
-        alloc_commit(SELF, ptr);                       \
-        mem_set(ptr, 0, SIZE *COUNT);                  \
-        ptr;                                           \
-    })
-
 #define alloc_make_array(SELF, T, COUNT) \
     ((T *)alloc_calloc(SELF, sizeof(T), COUNT))
 
@@ -51,3 +43,24 @@ struct alloc
 
 #define alloc_release(SELF, PTR) \
     ((SELF)->commit((SELF), PTR))
+
+#define alloc_malloc(SELF, SIZE) (             \
+    {                                          \
+        void *ptr = alloc_acquire(SELF, SIZE); \
+        alloc_commit(SELF, ptr);               \
+        ptr;                                   \
+    })
+
+#define alloc_free(SELF, PTR) (    \
+    {                              \
+        alloc_decommit(SELF, PTR); \
+        alloc_release(SELF, PTR);  \
+    })
+
+#define alloc_calloc(SELF, SIZE, COUNT) (              \
+    {                                                  \
+        void *ptr = alloc_acquire(SELF, SIZE * COUNT); \
+        alloc_commit(SELF, ptr);                       \
+        mem_set(ptr, 0, SIZE *COUNT);                  \
+        ptr;                                           \
+    })
