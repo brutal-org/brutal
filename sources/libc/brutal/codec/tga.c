aee867f0a25370d6acefc46222adbcb429346742
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-07-08 19:01:52 +0200

    code-tga: Improved encoder API.

--- sources/libc/brutal/codec/tga.c
+++ sources/libc/brutal/codec/tga.c
@@ -1,12 +1,12 @@
 #include <brutal/codec/tga.h>
 #include "brutal/base/endian.h"
 
-IoWriteResult tga_encode(IoWriter *writer, GfxSurface *surface)
+IoWriteResult tga_encode(IoWriter *writer, GfxSurface surface)
 {
     TgaHeader header = (TgaHeader){
         .data_type_code = le_cast(le_int8_t, TGA_DATATYPE_UNCOMPRESSED_RGB),
-        .width = le_cast(le_int16_t, surface->width),
-        .height = le_cast(le_int16_t, surface->height),
+        .width = le_cast(le_int16_t, surface.width),
+        .height = le_cast(le_int16_t, surface.height),
         .bits_per_pixel = le_cast(le_int8_t, 32),
         .image_descriptor = le_cast(le_int8_t, 0), // by default don't use an alpha channel
     };
@@ -19,7 +19,7 @@ IoWriteResult tga_encode(IoWriter *writer, GfxSurface *surface)
     size_t written = 0;
 
     written = TRY(IoWriteResult, io_write(writer, &header, sizeof(TgaHeader)));
-    written += TRY(IoWriteResult, io_write(writer, surface->buffer, surface->size));
+    written += TRY(IoWriteResult, io_write(writer, surface.buffer, surface.size));
 
     return OK(IoWriteResult, written);
 }
