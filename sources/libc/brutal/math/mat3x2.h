0847df882537114002021603f357c1d16c20d919
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-07-09 18:54:17 +0200

    brutal-math: Some rect and mat3x2 APIs.

--- sources/libc/brutal/math/mat3x2.h
+++ sources/libc/brutal/math/mat3x2.h
@@ -8,3 +8,15 @@
 
 typedef Mat3x2(int) Mat3x2i;
 typedef Mat3x2(float) Mat3x2f;
+
+#define mat3x2_indentity(T) \
+    (T)                     \
+    {                       \
+        .m = { 1.0f,        \
+               0.0f,        \
+               0.0f,        \
+               1.0f,        \
+               0.0f,        \
+               0.0f,        \
+        }                   \
+    }
--- sources/libc/brutal/math/rect.h
+++ sources/libc/brutal/math/rect.h
@@ -11,3 +11,6 @@
 
 typedef Rect(int) Recti;
 typedef Rect(float) Rectf;
+
+#define rect_cast(T, RECT) \
+    ((T){(RECT).x, (RECT).y, (RECT).w, (RECT).h})
