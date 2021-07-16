e9875944254d721effc1b138322c3ee6ae37688c
https://supercip971@github.com <supercyp@protonmail.com>
2021-07-16 11:03:07 -0400

    bid: restart + WIP + fix - squash some commits

--- sources/libraries/brutal/bid/bid.h
+++ sources/libraries/brutal/bid/bid.h
@@ -3,76 +3,61 @@
 #include <brutal/ds.h>
 #include <brutal/io/scan.h>
 
-enum bid_token_type
-{
-    BID_TOKEN_NULL,
-    BID_TOKEN_STR,
-    BID_TOKEN_KEYWORD,
-    BID_CURLY_START,
-    BID_CURLY_END,
-    BID_PARENTHESIS_START,
-    BID_PARENTHESIS_END,
-    BID_SQUARE_BRACKETS_START,
-    BID_SQUARE_BRACKETS_END,
-    BID_END_LINE,
-    BID_COLON,
-    BID_SEMICOLON,
-    BID_COMMA,
-    BID_ARROW,
-    BID_COMMENT_START,
-    BID_COMMENT_END,
-    BID_ASTERIX,
-    BID_DIGIT,
-};
-
-enum bid_keywords
-{
-    BID_STRUCT,
-    BID_VAR,
-    BID_METHOD,
-    BID_ENUM,
-    BID_INTERFACE,
-    BID_INCLUDE,
-};
+#define BID_INTERFACE_TOKEN (str_cast("interface"))
 
 enum bid_error_type
 {
-    BID_NO_ERROR,
-    BID_SCANNER_ERROR,
-    BID_TOKENIZATION_ERROR,
-    BID_INVALID_TOKEN,
-    BID_INVALID_KEYWORD,
-    BID_INVALID_INT,
-    BID_OTHER_ERROR,
+    BID_ERROR_TYPE_INVALID,
+};
+
+struct bid_buffer_position
+{
+    int offset;
 };
 
 struct bid_error
 {
     enum bid_error_type type;
-    int line;
-    int offset;
+    struct bid_buffer_position pos;
     Str message;
 };
 
-struct bid_token
+enum bid_ast_node_type
 {
-    enum bid_token_type type;
-    int line;
-    int offset;
-    long int_value;
-    Str raw_value;
+    BID_AST_NODE_TYPE_UNKNOWN,
+    BID_AST_NODE_TYPE_INTERFACE,
+};
+
+struct bid_ast_node
+{
+    enum bid_ast_node_type type;
+    Vec(struct bid_ast_node) children;
+
+    union
+    {
+        struct ast_node_interface
+        {
+            Str name;
+        } interface;
+
+    };
 };
 
 struct bid
 {
     Str in_data;
-    struct scan scanner;
-    Vec(struct bid_token) tokens;
-};
 
+    struct bid_ast_node root_ast;
+    struct scan scanner;
+    int _current_scanned_token_cursor;
+};
+typedef Result(struct bid_error, MonoState) BidParseResult;
 typedef Result(struct bid_error, struct bid) BidResult;
-typedef Result(struct bid_error, MonoState) BidScanResult;
 
 BidResult init_bid(Str idl_in);
 
 Str bid_to_c(struct bid from);
+
+enum bid_token_type bid_token_from_char(char from);
+
+enum bid_keywords bid_keyword_from_string(Str from);
