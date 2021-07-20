e89b8a001d58520538f3796aa6de3a90716412b2
https://supercip971@github.com <supercyp@protonmail.com>
2021-07-20 12:52:14 -0400

    brutal: add some utilities function for Str

--- sources/libraries/brutal/text/str.c
+++ sources/libraries/brutal/text/str.c
@@ -11,14 +11,14 @@ void str_rvs(Str str)
     }
 }
 
-bool str_eq(Str lhs, Str rhs)
+bool str_eq(const Str lhs, const Str rhs)
 {
     if (lhs.len != rhs.len)
     {
         return false;
     }
 
-    for (size_t i = 0; i < MIN(lhs.len, rhs.len); i++)
+    for (size_t i = 0; i < lhs.len; i++)
     {
         if (lhs.buffer[i] != rhs.buffer[i])
         {
@@ -27,4 +27,66 @@ bool str_eq(Str lhs, Str rhs)
     }
 
     return true;
+}
+
+int str_cnt(const Str lStr, const Str rStr)
+{
+
+    if (lStr.len < rStr.len)
+    {
+        return 0;
+    }
+
+    int count = 0;
+    for (size_t i = 0; i < lStr.len - rStr.len; i++)
+    {
+        auto offseted_lStr = str_cast_n(rStr.len, lStr.buffer + i);
+
+        if (str_eq(offseted_lStr, rStr))
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int str_last(const Str lStr, const Str rStr)
+{
+    if (lStr.len < rStr.len)
+    {
+        return 0;
+    }
+
+    int pos = -1;
+    for (size_t i = 0; i < lStr.len - rStr.len; i++)
+    {
+        auto offseted_lStr = str_cast_n(rStr.len, lStr.buffer + i);
+
+        if (str_eq(offseted_lStr, rStr))
+        {
+            pos = i;
+        }
+    }
+
+    return pos;
+}
+
+int str_first(const Str lStr, const Str rStr)
+{
+    if (lStr.len < rStr.len)
+    {
+        return 0;
+    }
+    for (size_t i = 0; i < lStr.len - rStr.len; i++)
+    {
+        auto offseted_lStr = str_cast_n(rStr.len, lStr.buffer + i);
+
+        if (str_eq(offseted_lStr, rStr))
+        {
+            return i;
+        }
+    }
+
+    return -1;
 }
