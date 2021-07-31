0266c26c9bea1cc73ce4ff2f3469ba6a99c7c9bf
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-07-31 12:25:40 +0200

    test: Writen more range_alloc test.

--- sources/tests/brutal/ds/range.c
+++ sources/tests/brutal/ds/range.c
@@ -12,11 +12,11 @@ TEST(ds_range_alloc_used_start)
 
     auto higher_half = (USizeRange){0x1000, 0x4000};
 
-    assert_equal(alloc.length, 1);
-    assert_truth(range_eq(alloc.data[0], higher_half));
-
     log("After test");
     range_alloc_dump(&alloc);
+
+    assert_equal(alloc.length, 1);
+    assert_truth(range_eq(alloc.data[0], higher_half));
 
     range_alloc_deinit(&alloc);
 }
@@ -32,12 +32,12 @@ TEST(ds_range_alloc_used_center)
     auto lower_half = (USizeRange){0x0, 0x2000};
     auto higher_half = (USizeRange){0x3000, 0x2000};
 
+    log("After test");
+    range_alloc_dump(&alloc);
+
     assert_equal(alloc.length, 2);
     assert_truth(range_eq(alloc.data[0], lower_half));
     assert_truth(range_eq(alloc.data[1], higher_half));
-
-    log("After test");
-    range_alloc_dump(&alloc);
 
     range_alloc_deinit(&alloc);
 }
@@ -52,11 +52,70 @@ TEST(ds_range_alloc_used_end)
 
     auto lower_half = (USizeRange){0x0, 0x4000};
 
+    log("After test");
+    range_alloc_dump(&alloc);
+
     assert_equal(alloc.length, 1);
     assert_truth(range_eq(alloc.data[0], lower_half));
 
+    range_alloc_deinit(&alloc);
+}
+
+TEST(ds_range_alloc_unused_begin)
+{
+    RangeAlloc alloc;
+    range_alloc_init(&alloc, alloc_global());
+
+    range_alloc_unused(&alloc, (USizeRange){0x0, 0x1000});
+    range_alloc_unused(&alloc, (USizeRange){0x1000, 0x1000});
+
+    auto final_range = (USizeRange){0x0, 0x2000};
+
     log("After test");
     range_alloc_dump(&alloc);
+
+    assert_equal(alloc.length, 1);
+    assert_truth(range_eq(alloc.data[0], final_range));
+
+    range_alloc_deinit(&alloc);
+}
+
+TEST(ds_range_alloc_unused_middle)
+{
+    RangeAlloc alloc;
+    range_alloc_init(&alloc, alloc_global());
+
+    range_alloc_unused(&alloc, (USizeRange){0x0, 0x1000});
+    range_alloc_unused(&alloc, (USizeRange){0x2000, 0x1000});
+    range_alloc_unused(&alloc, (USizeRange){0x1000, 0x1000});
+
+    auto final_range = (USizeRange){0x0, 0x3000};
+
+    log("After test");
+    range_alloc_dump(&alloc);
+
+    assert_equal(alloc.length, 1);
+    assert_truth(range_eq(alloc.data[0], final_range));
+
+    range_alloc_deinit(&alloc);
+}
+
+TEST(ds_range_alloc_unused_end)
+{
+    RangeAlloc alloc;
+    range_alloc_init(&alloc, alloc_global());
+
+    range_alloc_unused(&alloc, (USizeRange){0x0, 0x1000});
+    range_alloc_unused(&alloc, (USizeRange){0x1000, 0x1000});
+    range_alloc_unused(&alloc, (USizeRange){0x2000, 0x1000});
+
+    auto final_range = (USizeRange){0x0, 0x3000};
+
+    log("After test");
+    range_alloc_dump(&alloc);
+
+    assert_equal(alloc.length, 1);
+    assert_truth(range_eq(alloc.data[0], final_range));
 
     range_alloc_deinit(&alloc);
 }
