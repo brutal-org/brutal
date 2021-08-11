6e2a6b7b1af1a952789f5aefe76ba76a94374d91
Abb1x <abbix@riseup.net>
2021-08-11 19:24:50 -0400

    brutal-boot: add panic()

--- sources/libs/efi/utils.h
+++ sources/libs/efi/utils.h
@@ -23,11 +23,26 @@ EFIFileInfo get_file_info(File *file);
 void efi_printf(char *fmt, ...);
 void *efi_malloc(u64 size);
 void efi_free(void *ptr);
+void clear_screen();
+void set_attribute(u64 attr);
+void panic(char *str, ...);
 
 #define TO_UTF16(ptr, buffer)                                     \
-    void *ptr = efi_malloc(strlen(buffer) << 1);                 \
-    memset(ptr, 0, sizeof(ptr));                                 \
+    void *ptr = efi_malloc(strlen(buffer) << 1);                  \
+    memset(ptr, 0, sizeof(ptr));                                  \
     for (size_t i = 0; i < sizeof(buffer) / sizeof(*buffer); i++) \
     {                                                             \
         *(char16 *)(ptr + (i << 1)) = buffer[i];                  \
     }
+
+#define UNWRAP_WITH_MSG(expr, msg, ...) (           \
+    {                                               \
+        auto expr_value = (expr);                   \
+                                                    \
+        if (!expr_value.success)                    \
+        {                                           \
+            panic(msg, __VA_OPT__(, ) __VA_ARGS__); \
+        }                                           \
+                                                    \
+        expr_value._ok;                             \
+    })
