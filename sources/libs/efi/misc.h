2aa9b2d8db33e73e316481e71d9bf284b2547d45
Abb1x <abbix@riseup.net>
2021-08-12 15:12:29 -0400

    brutal-efi: remove vsprintf

--- sources/libs/efi/misc.h
+++ sources/libs/efi/misc.h
@@ -1,5 +1,4 @@
-#ifndef __CLIB_H__
-#define __CLIB_H__
+#pragma once
 
 #include <ctype.h>
 #include <stdarg.h>
@@ -8,10 +7,5 @@
 #include <string.h>
 
 int u16strcmp(const uint16_t *l, const uint16_t *r);
-
-int u16vsnprintf(uint16_t *buffer, size_t size, const char *fmt, va_list args);
-int u16snprintf(uint16_t *buffer, size_t size, const char *fmt, ...);
 size_t u16strlen(const uint16_t *str);
 uint16_t *to_u16strncpy(uint16_t *dst, const char *src, size_t size);
-
-#endif // __CLIB_H__
