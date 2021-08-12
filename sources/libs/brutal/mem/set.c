e82b894c631938fac7e20c137c2280d27a5b95bc
supercip971 <supercyp971@gmail.com>
2021-08-12 20:20:42 +0200

    brutal: use stosb for mem_set

--- sources/libs/brutal/mem/set.c
+++ sources/libs/brutal/mem/set.c
@@ -1,13 +1,32 @@
 #include <brutal/mem.h>
 
+#if defined(__x86_64__) || defined(__x86_32__)
+
+void __stosb_set(void* d, uint8_t s, size_t c)
+{
+    asm volatile (
+        "rep stosb"
+        :"=D"(d),"=c"(c)
+        :"0"(d),"a"(s),"1"(c)
+        :"memory"
+    );
+}
+
+#endif
+
 void *mem_set(void *s, uint8_t c, size_t n)
 {
+
+#if defined(__x86_64__) || defined(__x86_32__)
+
+    __stosb_set(s, c, n);
+#else
     uint8_t *dest = (uint8_t *)s;
 
     for (size_t i = 0; i < n; i++)
     {
         dest[i] = c;
     }
-
+#endif
     return s;
 }
