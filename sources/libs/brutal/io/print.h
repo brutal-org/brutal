d6eaa4a1407c8427d0da1bb7b66041a057bcd931
Abb1x <abbix@riseup.net>
2021-08-13 09:43:30 -0400

    brutal-boot: cleanup

--- sources/libs/brutal/io/print.h
+++ sources/libs/brutal/io/print.h
@@ -58,10 +58,9 @@ struct print_value print_val_pointer(void *);
         signed long: print_val_signed,        \
                                               \
         unsigned char: print_val_unsigned,    \
-        unsigned short: print_val_unsigned,    \
+        unsigned short: print_val_unsigned,   \
         unsigned int: print_val_unsigned,     \
         unsigned long: print_val_unsigned,    \
-                                              \
         char*: print_val_cstring,             \
         char: print_val_char,                 \
         Str: print_val_string,                \
--- sources/loader/test/test.c
+++ sources/loader/test/test.c
@@ -1,4 +1,75 @@
+#include <stddef.h>
+#include <stdint.h>
+
+uint8_t inb(uint16_t port)
+{
+    uint8_t data;
+    __asm__ volatile("inb %1, %0"
+                     : "=a"(data)
+                     : "d"(port));
+    return data;
+}
+
+void outb(uint16_t port, uint8_t data)
+{
+    __asm__ volatile("outb %0, %1"
+                     :
+                     : "a"(data), "Nd"(port));
+}
+
+typedef enum
+{
+    COM1 = 0x3F8,
+    COM2 = 0x2F8,
+    COM3 = 0x3E8,
+    COM4 = 0x2E8
+} SerialPort;
+
+static int is_transmit_empty(SerialPort port)
+{
+    return inb(port + 5) & 0x20;
+}
+
+static int serial_received(SerialPort port)
+{
+    return inb(port + 5) & 1;
+}
+
+void com_initialize(SerialPort port)
+{
+    outb(port + 1, 0x00);
+    outb(port + 3, 0x80);
+    outb(port + 0, 0x03);
+    outb(port + 1, 0x00);
+    outb(port + 3, 0x03);
+    outb(port + 2, 0xC7);
+    outb(port + 4, 0x0B);
+}
+
+void com_putc(SerialPort port, char c)
+{
+    while (is_transmit_empty(port) == 0)
+        ;
+    outb(port, c);
+}
+
+void com_write_string(SerialPort port, char *str)
+{
+    while (*str)
+    {
+        com_putc(port, *str++);
+    }
+}
+
+char com_getc(SerialPort port)
+{
+    while (serial_received(port) == 0)
+        ;
+    return inb(port);
+}
+
 int main(void)
 {
-  return 42;
+    com_write_string(COM1, "Hello world!");
+    return 42;
 }
