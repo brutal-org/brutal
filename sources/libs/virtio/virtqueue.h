04ef4a9e4525eb251e7b03d13c26ffa974a85fb8
Quentincestino <quentindutilleul@protonmail.com>
2021-08-16 23:17:04 +0200

    VirtIO: Starting the virtqueue implementation

--- sources/libs/virtio/virtqueue.h
+++ sources/libs/virtio/virtqueue.h
@@ -1,5 +1,14 @@
 #pragma once
 
-typedef struct
+#include <brutal/base.h>
+#include <virtio/splitvqueue.h>
+
+// The descriptor of one buffer inside the queue.
+typedef struct PACKED
 {
-} VirtioQueue;
+    le_uint64_t address;
+    le_uint32_t len;
+
+    le_uint16_t flags;
+    le_uint16_t next;
+} VirtioVQueueDesc;
