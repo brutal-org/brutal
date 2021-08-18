a64f8622dafc165ff94662440a49f5beda305b42
https://supercip971@github.com <supercyp@protonmail.com>
2021-08-18 12:28:46 -0400

    libjson: add test + tyding up

--- sources/libs/json/json.c
+++ sources/libs/json/json.c
@@ -1,156 +1,116 @@
 #include <ansi/ctype.h>
 #include <json/json.h>
 
-static bool json_parse_value(Scan *scan, Alloc *alloc, struct json_object *from);
-
 static int is_closing_string(int c)
 {
-    if (c == '"')
-    {
-        return false;
-    }
-    return true;
+    return c != '"';
 }
 
 static Str json_parse_str(Scan *scan)
 {
+    scan_skip_space(scan);
     scan_expect(scan, '"');
 
     Str name = scan_skip_until(scan, is_closing_string);
 
     scan_expect(scan, '"');
+    scan_skip_space(scan);
 
     return name;
 }
 
-static char json_parse_char(Scan *scan)
+static JsonValue json_parse_array(Scan *scan, Alloc *alloc)
 {
-    scan_expect(scan, '\'');
-
-    char res = scan_next(scan);
-
-    scan_expect(scan, '\'');
-
-    return res;
-}
-
-static void json_parse_array(Scan *scan, Alloc *alloc, struct json_object *from)
-{
-    vec_init(&from->array.elements, alloc);
+    JsonValue ret;
+    ret.type = JSON_ARRAY;
+    vec_init(&ret.array, alloc);
 
     while (scan_curr(scan) != ']' && !scan_end(scan))
     {
-        struct json_object res;
+        JsonValue res;
 
-        if (!json_parse_value(scan, alloc, &res))
-        {
-            scan_throw(scan, str_cast("expected a value"), str_cast(""));
-            return;
-        }
+        res = json_parse(scan, alloc);
 
         scan_skip_space(scan);
 
-        vec_push(&from->array.elements, res);
+        vec_push(&ret.array, res);
 
         if (!scan_skip(scan, ','))
         {
-            return;
+            return ret;
         }
     }
+
+    return ret;
 }
 
-static void json_parse_member(Scan *scan, Alloc *alloc, json_members *from)
+static JsonValue json_parse_object(Scan *scan, Alloc *alloc)
 {
-    Str name = json_parse_str(scan);
-    scan_skip_space(scan);
-    scan_expect(scan, ':');
-
-    struct json_object value;
-    if (!json_parse_value(scan, alloc, &value))
-    {
-        scan_throw(scan, str_cast("expected a value"), str_cast(""));
-        return;
-    }
-
-    map_put(from, name, value);
-}
-
-static void json_parse_object(Scan *scan, Alloc *alloc, struct json_object *from)
-{
-    map_init(&from->object.members, alloc);
+    JsonValue ret;
+    ret.type = JSON_OBJECT;
+    map_init(&ret.object, alloc);
 
     while (scan_curr(scan) != '}' && !scan_end(scan))
     {
+        Str name = json_parse_str(scan);
+
+        scan_expect(scan, ':');
+
+        JsonValue value = json_parse(scan, alloc);
+
+        map_put(&ret.object, name, value);
+
         scan_skip_space(scan);
-
-        json_parse_member(scan, alloc, &from->object.members);
-
-        scan_skip_space(scan);
-
         if (!scan_skip(scan, ','))
         {
-            return;
+            return ret;
         }
     }
+    return ret;
 }
 
-static bool json_parse_value(Scan *scan, Alloc *alloc, struct json_object *from)
+JsonValue json_parse(Scan *scan, Alloc *alloc)
 {
+    JsonValue from;
     scan_skip_space(scan);
 
     if (scan_curr(scan) == '"') // string
     {
-        from->type = JSON_VALUE_STRING;
-        from->string = json_parse_str(scan);
-    }
-    else if (scan_curr(scan) == '\'') // char
-    {
-        from->type = JSON_VALUE_CHAR;
-        from->character = json_parse_char(scan);
+        from.type = JSON_STRING;
+        from.string = json_parse_str(scan);
     }
     else if (isdigit(scan_curr(scan)) || scan_curr(scan) == '-') // number
     {
-        from->type = JSON_VALUE_NUMBER;
-        from->number = scan_next_decimal(scan);
+        from.type = JSON_NUMBER;
+        from.number = scan_next_decimal(scan);
     }
     else if (scan_skip_word(scan, str_cast("true"))) // boolean (true)
     {
-        from->type = JSON_VALUE_BOOL;
-        from->boolean = true;
+        from.type = JSON_BOOL;
+        from.boolean = true;
     }
     else if (scan_skip_word(scan, str_cast("false"))) // boolean (false)
     {
-        from->type = JSON_VALUE_BOOL;
-        from->boolean = false;
+        from.type = JSON_BOOL;
+        from.boolean = false;
     }
     else if (scan_skip_word(scan, str_cast("null"))) // boolean (false)
     {
-        from->type = JSON_VALUE_NULL;
+        from.type = JSON_NULL;
     }
     else if (scan_skip(scan, '{')) // object
     {
-        from->type = JSON_OBJECT;
-        json_parse_object(scan, alloc, from);
+        from = json_parse_object(scan, alloc);
     }
     else if (scan_skip(scan, '[')) // array
     {
-        from->type = JSON_ARRAY;
-        json_parse_array(scan, alloc, from);
+        from = json_parse_array(scan, alloc);
     }
     else
     {
-        return false;
+        from.type = JSON_ERROR;
+        scan_throw(scan, str_cast("expected a value"), str_cast(""));
     }
 
-    return true;
-}
-
-struct json_object json_parse(Scan *scan, Alloc *alloc)
-{
-
-    struct json_object root_object = {.type = JSON_OBJECT};
-    scan_skip_space(scan);
-    json_parse_value(scan, alloc, &root_object);
-
-    return root_object;
+    return from;
 }
---
+++ sources/tests/json/json.c
@@ -0,0 +1,115 @@
+#include <brutal/alloc.h>
+#include <brutal/alloc/heap.h>
+#include <brutal/gfx.h>
+#include <brutal/io.h>
+#include <json/json.h>
+#include "brutal/log/assert.h"
+#include "tests/test.h"
+
+#define JSON_TEST_START()                                      \
+    struct alloc_heap heap;                                    \
+    alloc_heap_init(&heap);                                    \
+    Scan scanner = {};                                         \
+    scan_init(&scanner, base);                                 \
+    JsonValue json = json_parse(&scanner, &heap.base); \
+    assert_equal((int)json.type, (int)JSON_OBJECT)
+
+#define JSON_TEST_END() \
+    alloc_heap_deinit(&heap);
+
+#define JSON_MEMBER_GET(_object, _type, _name)                      \
+    (                                                               \
+        {                                                           \
+            JsonValue _res = {};                            \
+            assert_truth(map_get(&_object.object, _name, &(_res))); \
+            assert_equal((int)_res.type, (int)_type);               \
+            _res;                                                   \
+        })
+
+TEST(json_parse_test)
+{
+    Str base = str_cast("{ \"hello\": \"world\" }");
+
+    JSON_TEST_START();
+
+    JsonValue res = JSON_MEMBER_GET(json, JSON_STRING, str_cast("hello"));
+
+    assert_str_equal(res.string, str_cast("world"));
+
+    JSON_TEST_END();
+}
+
+TEST(json_parse_int)
+{
+    Str base = str_cast("{ \"hello\": 10 }");
+
+    JSON_TEST_START();
+
+    JsonValue res = JSON_MEMBER_GET(json, JSON_NUMBER, str_cast("hello"));
+
+    assert_equal(res.number, 10);
+
+    JSON_TEST_END();
+}
+
+TEST(json_parse_false)
+{
+    Str base = str_cast("{ \"hello\": false }");
+
+    JSON_TEST_START();
+
+    JsonValue res = JSON_MEMBER_GET(json, JSON_BOOL, str_cast("hello"));
+
+    assert_equal((int)res.boolean, (int)false);
+
+    JSON_TEST_END();
+}
+
+TEST(json_parse_true)
+{
+    Str base = str_cast("{ \"hello\": true }");
+
+    JSON_TEST_START();
+
+    JsonValue res = JSON_MEMBER_GET(json, JSON_BOOL, str_cast("hello"));
+
+    assert_equal((int)res.boolean, (int)true);
+
+    JSON_TEST_END();
+}
+
+TEST(json_parse_object_in_object)
+{
+    Str base = str_cast("{ \"hello\": { \"oh\": \"rust\", \"is\": \"bad\" } }");
+
+    JSON_TEST_START();
+
+    JsonValue res = JSON_MEMBER_GET(json, JSON_OBJECT, str_cast("hello"));
+
+    JsonValue oh = JSON_MEMBER_GET(res, JSON_STRING, str_cast("oh"));
+
+    assert_str_equal(oh.string, str_cast("rust"));
+
+    JsonValue is = JSON_MEMBER_GET(res, JSON_STRING, str_cast("is"));
+
+    assert_str_equal(is.string, str_cast("bad"));
+    JSON_TEST_END();
+}
+
+TEST(json_parse_table)
+{
+    Str base = str_cast("{ \"hello\": [ \"oh\", \"rust\", \"is\", \"bad\" ] }");
+
+    JSON_TEST_START();
+
+    JsonValue res = JSON_MEMBER_GET(json, JSON_ARRAY, str_cast("hello"));
+
+    assert_equal(res.array.length, 4);
+
+    assert_str_equal(res.array.data[0].string, str_cast("oh"));
+    assert_str_equal(res.array.data[1].string, str_cast("rust"));
+    assert_str_equal(res.array.data[2].string, str_cast("is"));
+    assert_str_equal(res.array.data[3].string, str_cast("bad"));
+
+    JSON_TEST_END();
+}
