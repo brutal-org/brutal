9d082863a9b18c0e41186febc64017c278627fca
https://supercip971@github.com <supercyp@protonmail.com>
2021-08-23 10:21:08 -0400

    brutal: double linked list tyding up

--- sources/libs/brutal/ds/double_linked_list.h
+++ sources/libs/brutal/ds/double_linked_list.h
@@ -17,11 +17,11 @@ typedef struct linked_list_impl_entry_t
 } LinkedListImpl;
 
 void linked_list_init_impl(LinkedListImpl *impl, Alloc *alloc);
+
 void linked_list_deinit_impl(LinkedListImpl *impl);
+
 LinkedListImpl *linked_list_insert_impl(LinkedListImpl *impl, int data_size);
 
-// return the new head if needed
-// if you delete all the list entry you will have a NULL returned
 void linked_list_remove_impl(LinkedListImpl *head, LinkedListImpl *impl);
 
 #define linked_list(T)        \
@@ -37,22 +37,22 @@ void linked_list_remove_impl(LinkedListImpl *head, LinkedListImpl *impl);
         LinkedListImpl _impl; \
     }
 
-#define linked_list_init(list, alloc) \
-    linked_list_init_impl(impl$(list), (alloc))
+#define linked_list_init(SELF, ALLOC) \
+    linked_list_init_impl(impl$(SELF), (ALLOC))
 
-#define linked_list_deinit(list) \
-    linked_list_deinit_impl(impl$(list))
+#define linked_list_deinit(SELF) \
+    linked_list_deinit_impl(impl$(SELF))
 
-#define linked_list_insert(list, entry) (                                             \
+#define linked_list_insert(SELF, ENTRY) (                                             \
     {                                                                                 \
-        auto res = (typeof(list))linked_list_insert_impl(impl$(list), sizeof(entry)); \
-        (*res->data) = entry;                                                         \
+        auto res = (typeof(SELF))linked_list_insert_impl(impl$(SELF), sizeof(ENTRY)); \
+        (*res->data) = ENTRY;                                                         \
         res;                                                                          \
     })
 
-#define linked_list_loop(LIST, ENTRY_NAME)                                                        \
-    for (auto ENTRY_NAME = (LIST); ENTRY_NAME; ENTRY_NAME = (typeof(ENTRY_NAME))ENTRY_NAME->next) \
-        if (ENTRY_NAME->data != nullptr)
+#define linked_list_loop(VAR, SELF)                            \
+    for (auto VAR = (SELF); VAR; VAR = (typeof(VAR))VAR->next) \
+        if (VAR->data != nullptr)
 
-#define linked_list_remove(LIST, ENTRY) \
-    linked_list_remove_impl(impl$(LIST), impl$(ENTRY))
+#define linked_list_remove(SELF, ENTRY) \
+    linked_list_remove_impl(impl$(SELF), impl$(ENTRY))
