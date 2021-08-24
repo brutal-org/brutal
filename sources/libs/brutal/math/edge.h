5fbc60c8bb6f12bd24c8157164f705e9ca516098
https://supercip971@github.com <supercyp@protonmail.com>
2021-08-24 10:05:24 -0400

    brutal: improved math lib - added some vec2 operation function - added types for double - added rect_collide_point

--- sources/libs/brutal/math/edge.h
+++ sources/libs/brutal/math/edge.h
@@ -11,6 +11,7 @@
 
 typedef Edge(Vec2i) Edgei;
 typedef Edge(Vec2f) Edgef;
+typedef Edge(Vec2d) Edged;
 
 #define edge_create(T, start, end) \
     ((T){start, end})
--- sources/libs/brutal/math/vec2.h
+++ sources/libs/brutal/math/vec2.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <math/math.h>
+
 #define Vec2(T) \
     struct      \
     {           \
@@ -9,9 +11,49 @@
 
 typedef Vec2(int) Vec2i;
 typedef Vec2(float) Vec2f;
+typedef Vec2(double) Vec2d;
 
 #define vec_create(T, x, y) \
     ((T){(x), (y)})
 
 #define vec$(T, VEC) \
     vec_create((T), (VEC).x, (VEC).y)
+
+#define vec2_add(a, b) ( \
+    {                    \
+        auto c = (a);    \
+        c.x += (b).x;    \
+        c.y += (b).y;    \
+        c;               \
+    })
+
+#define vec2_sub(a, b) ( \
+    {                    \
+        auto c = (a);    \
+        c.x -= (b).x;    \
+        c.y -= (b).y;    \
+        c;               \
+    })
+
+#define vec2_mul(a, b) ( \
+    {                    \
+        auto c = (a);    \
+        c.x *= (b).x;    \
+        c.y *= (b).y;    \
+        c;               \
+    })
+
+#define vec2_div(a, b) ( \
+    {                    \
+        auto c = (a);    \
+        c.x /= (b).x;    \
+        c.y /= (b).y;    \
+        c;               \
+    })
+
+#define vec2_distance(a, b) (                                          \
+    {                                                                  \
+        auto root = pow(((a).x - (b).x), 2) + pow(((a).y - (b).y), 2); \
+        root = sqrt(root);                                             \
+        root;                                                          \
+    })
