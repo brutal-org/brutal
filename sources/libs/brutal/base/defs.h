9c837e9f3df5eb8cc0fe0035d2e580f19f69a18d
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-09-07 15:08:53 +0200

    brutal-base: Improved compiler detection.

--- sources/libs/brutal/base/defs.h
+++ sources/libs/brutal/base/defs.h
@@ -3,14 +3,12 @@
 #define COMPILER_GCC 0
 #define COMPILER_CLANG 0
 
-#if defined(__GNUC__)
-#    if defined(__clang__)
-#        undef COMPILER_CLANG
-#        define COMPILER_CLANG 1
-#    else
-#        undef COMPILER_GCC
-#        define COMPILER_GCC 1
-#    endif
+#if (defined(__GNUC__) || defined(_MSC_VER)) && defined(__clang__)
+#    undef COMPILER_CLANG
+#    define COMPILER_CLANG 1
+#elif defined(__GNUC__)
+#    undef COMPILER_GCC
+#    define COMPILER_GCC 1
 #else
 #    error "Unsupported compiler!"
 #endif
