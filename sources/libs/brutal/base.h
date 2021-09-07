a76bb9aa3d2a9b6d4236ed4e3cb104828578c2ab
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-09-07 14:55:27 +0200

    library: Fix format in base.h.

--- sources/libs/brutal/base.h
+++ sources/libs/brutal/base.h
@@ -4,8 +4,10 @@
 #include <brutal/base/banned.h>
 #include <brutal/base/cast.h>
 #include <brutal/base/count.h>
+#include <brutal/base/defs.h>
 #include <brutal/base/endian.h>
 #include <brutal/base/error.h>
+#include <brutal/base/ints.h>
 #include <brutal/base/iteration.h>
 #include <brutal/base/keywords.h>
 #include <brutal/base/macros.h>
@@ -13,5 +15,3 @@
 #include <brutal/base/result.h>
 #include <brutal/base/slice.h>
 #include <brutal/base/std.h>
-#include <brutal/base/ints.h>
-#include <brutal/base/defs.h>
