d3a7a780783cc08f10d8cac61f27020c8d95ec2c
Quentincestino <38379985+Quentincestino@users.noreply.github.com>
2021-09-07 15:21:04 +0200

    virtio: Starting VirtIO implementation.
    * VirtIO: Added the virtio lib on build.mk
    
    * VirtIO: Started a virtio lib
    
    * VirtIO: Changed `VirtioDeviceClass` for `VirtioDeviceInit`
    
    * VirtIO: Starting the virtqueue system
    
    * VirtIO: Starting a block device implementation to make choices in API
    
    * VirtIO: General purpose features
    
    * VirtIO: Fixing disgusting API choice
    
    * VirtIO: Starting the virtqueue implementation
    
    * virtio: idk
    
    * VirtIO: added header guard
    
    * VirtIO: Removed embedded structure
    
    * VirtIO: Replaced blk requests' defines to enum
    
    * VirtIO: Use bitshift instead of full binary representation
    
    * meta: remove virtio's pdf spec from gitignore
    
    * make: removed unused makefile

---
+++ sources/libs/virtio/block_device.c
@@ -0,0 +1,21 @@
+#include <virtio/block_device.h>
+#include <virtio/device.h>
+#include <virtio/virtqueue.h>
+
+VirtioDeviceResult block_setup(VirtioDevice *device)
+{
+    // TODO: VirtIO block device initialization...
+    return VIRTIO_DEVICE_OK;
+}
+
+VirtioBlockDevice virtio_block_init(VirtioDevice *device, uint64_t needed_features)
+{
+    VirtioDeviceInit device_init = {
+        .device_type = VIRTIO_BLOCK,
+        .needed_features = needed_features,
+    };
+
+    virtio_device_init(device, &device_init);
+
+    return (VirtioBlockDevice){};
+}
