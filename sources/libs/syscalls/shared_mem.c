8c8d5c444e541c1906960dcfbaed57b95bbadc7f
Supercip971 <supercyp971@gmail.com>
2021-09-30 11:11:41 -0400

    brutal: added shared memory utilities functions

---
+++ sources/libs/syscalls/shared_mem.c
@@ -0,0 +1,78 @@
+#include <brutal/log.h>
+#include <syscalls/shared_mem.h>
+
+void *shared_mem_map(SharedMem *self)
+{
+    if (self->data != NULL)
+    {
+        return self->data->data;
+    }
+
+    BrMapArgs map_args = {
+        .space = BR_SPACE_SELF,
+        .mem_obj = self->obj,
+        .flags = BR_MEM_WRITABLE,
+    };
+
+    br_map(&map_args);
+
+    self->map = map_args;
+    self->data = (SharedMemData *)self->map.vaddr;
+
+    return self->data->data;
+}
+
+SharedMem shared_mem_alloc(size_t size)
+{
+    SharedMem result = {};
+
+    BrCreateArgs mem_obj_args = {
+        .type = BR_OBJECT_MEMORY,
+        .mem_obj = {
+            .size = size + sizeof(SharedMemData),
+        },
+    };
+
+    assert_br_success(br_create(&mem_obj_args));
+
+    result.obj = mem_obj_args.handle;
+
+    shared_mem_map(&result);
+
+    result.data->len = size;
+    return result;
+}
+
+void shared_mem_free(SharedMem *self)
+{
+    BrUnmapArgs unmap_args = {
+        .space = BR_SPACE_SELF,
+        .size = self->map.size,
+        .vaddr = self->map.vaddr,
+    };
+
+    br_unmap(&unmap_args);
+    self->data = NULL;
+}
+
+void shared_mem_destroy(SharedMem *self)
+{
+    BrCloseArgs close_args = {
+        .handle = self->obj,
+    };
+
+    br_close(&close_args);
+}
+SharedMem memobj_to_shared(BrMemObj obj)
+{
+    SharedMem mem = {
+        .obj = obj,
+    };
+
+    return mem;
+}
+
+BrMemObj shared_to_memobj(SharedMem from)
+{
+    return from.obj;
+}
---
+++ sources/libs/syscalls/shared_mem.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <stddef.h>
+#include <stdint.h>
+#include <syscalls/syscalls.h>
+
+typedef struct
+{
+    size_t len;
+    uint8_t data[];
+} SharedMemData;
+
+typedef struct
+{
+    BrMemObj obj;
+    BrMapArgs map;
+    SharedMemData *data;
+} SharedMem;
+
+SharedMem memobj_to_shared(BrMemObj obj);
+
+BrMemObj shared_to_memobj(SharedMem from);
+
+// shared_mem_alloc also map <!>
+SharedMem shared_mem_alloc(size_t size);
+
+void shared_mem_free(SharedMem *self);
+
+// use only when you receive it, shared_mem_alloc already call it
+void *shared_mem_map(SharedMem *self);
+
+void shared_mem_unmap(SharedMem *self);
