06d53e7378d4676a62d43fa50a259a2625f0bf3d
Supercip971 <supercyp971@gmail.com>
2021-10-04 10:28:16 -0400

    vfs: first vfs iteration

---
+++ sources/bins/vfs/fs.c
@@ -0,0 +1,103 @@
+#include "vfs/fs.h"
+#include <ansi/string.h>
+#include <brutal/alloc.h>
+#include <brutal/ds.h>
+#include <brutal/log.h>
+#include <syscalls/shared_mem.h>
+
+VfsFileSystemHandle fs_next_handle = 1;
+Vec(VfsFileSystem) devices;
+
+void vfs_fs_init(void)
+{
+    vec_init(&devices, alloc_global());
+}
+
+// FIXME: this function is dumb and we should implement a better one, but for the moment it works.
+VfsFileSystemHandle get_mounted_fs(Str path)
+{
+    VfsFileSystemHandle raw_id = 0;
+    size_t cur_path_size = 0;
+    vec_foreach(v, &devices)
+    {
+        if (cur_path_size < v.mount_point.len)
+        {
+            if (strncmp(path.buffer, v.mount_point.buffer, v.mount_point.len) == 0)
+            {
+                raw_id = v.self_handle;
+            }
+        }
+    }
+    return raw_id;
+}
+
+VfsFileSystem *get_fs(VfsFileSystemHandle handle)
+{
+    for (int i = 0; i < devices.length; i++)
+    {
+        if (devices.data[i].self_handle == handle)
+        {
+            return devices.data + i;
+        }
+    }
+    return nullptr;
+}
+
+VfsFileSystemHandle mount(BrHandle fs_task, SharedMem *path)
+{
+    VfsFileSystem dev;
+    Str path_str = shared_mem_to_str(path);
+    dev.self_handle = fs_next_handle++;
+    dev.task = fs_task;
+    dev.mount_point = str_dup(path_str, alloc_global());
+
+    vec_push(&devices, dev);
+    FsdevMountRequest req =
+        {
+            .path = path->obj};
+    FsdevMountResponse resp = {};
+    fsdev_mount(fs_task, &req, &resp);
+    log$("mounted fs ({}) at: {}", fs_task, dev.mount_point);
+
+    if (resp.accepted)
+    {
+        log$("accepted! ");
+    }
+    else
+    {
+        log$("error: can't mount");
+    }
+    return dev.self_handle;
+}
+
+BrResult unmount(Str path)
+{
+    panic_todo$("implement unmount ! ({})", path);
+}
+
+FsdevInode fs_open(SharedMem path, VfsFileSystem *self)
+{
+    FsdevOpenRequest req = {
+        .path = path.obj,
+    };
+
+    FsdevOpenResponse resp = {};
+    fsdev_open(self->task, &req, &resp);
+
+    return resp.handle;
+}
+
+size_t fs_write(size_t off, FsdevInode inode, BrMemObj dat, VfsFileSystem *self)
+{
+    FsdevWriteRequest req = {
+        .off = off,
+        .handle = inode,
+        .data = dat,
+    };
+
+    FsdevWriteResponse resp = {};
+
+    fsdev_write(self->task, &req, &resp);
+
+    return resp.written;
+}
---
+++ sources/bins/vfs/vfs.c
@@ -0,0 +1,130 @@
+#include "vfs/vfs.h"
+#include <brutal/alloc.h>
+#include <brutal/log.h>
+#include <brutal/mem.h>
+#include "vfs/fs.h"
+
+VfsFileHandle file_next_handle = 1;
+
+Vec(VfsTask) tasks_fd;
+void vfs_init(void)
+{
+    vec_init(&tasks_fd, alloc_global());
+}
+
+VfsTask *get_task_fds(TaskId id)
+{
+    for (int i = 0; i < tasks_fd.length; i++)
+    {
+        if (tasks_fd.data[i].id == id)
+        {
+            return tasks_fd.data + i;
+        }
+    }
+    return NULL;
+}
+
+VfsTask *get_task_fds_or_create(TaskId id)
+{
+    VfsTask *r = get_task_fds(id);
+
+    if (r == nullptr)
+    {
+
+        VfsTask v = {};
+        vec_init(&v.openned_files, alloc_global());
+
+        v.active = true;
+        v.id = id;
+
+        vec_push(&tasks_fd, v);
+        return get_task_fds(id);
+    }
+
+    return r;
+}
+
+void add_file(VfsFile *file, VfsTask *self)
+{
+
+    for (int i = 0; i < self->openned_files.length; i++)
+    {
+        if (self->openned_files.data[i].status == FS_FREE || self->openned_files.data[i].status == FS_CLOSED)
+        {
+            self->openned_files.data[i] = *file;
+            return;
+        }
+    }
+
+    vec_push(&self->openned_files, *file);
+}
+
+VfsFile *get_file(VfsFileHandle handle, VfsTask *self)
+{
+    for (int i = 0; i < self->openned_files.length; i++)
+    {
+        if (self->openned_files.data[i].handle == handle)
+        {
+            return self->openned_files.data + i;
+        }
+    }
+
+    return nullptr;
+}
+
+VfsError open(SharedMem path, TaskId opener, VfsFileHandle *result)
+{
+
+    Str path_m = shared_mem_to_str(&path);
+    VfsFileSystemHandle file_system = get_mounted_fs(path_m);
+
+    if (file_system == 0)
+    {
+        return VFS_INVALID_PATH;
+    }
+    auto mount = get_fs(file_system);
+    if (mount == nullptr)
+    {
+        return VFS_INVALID_PATH;
+    }
+
+    auto inode = fs_open(path, mount);
+    VfsFile file = {
+        .handle = file_next_handle++,
+        .fs_handle = file_system,
+        .fs_inode = inode,
+        .path = str_dup(path_m, alloc_global()),
+        .status = FS_OPENED,
+    };
+
+    VfsTask *t = get_task_fds_or_create(opener);
+
+    add_file(&file, t);
+
+    *result = file.handle;
+
+    return VFS_SUCCESS;
+}
+
+VfsError write(VfsFileHandle handle, TaskId opener, BrMemObj obj, size_t off, size_t *res)
+{
+    VfsTask *t = get_task_fds(opener);
+
+    if (t == nullptr)
+    {
+        return VFS_INVALID_HANDLE;
+    }
+
+    VfsFile* f = get_file(handle, t);
+
+    if (f == nullptr)
+    {
+        return VFS_INVALID_HANDLE;
+    }
+
+    auto mount = get_fs(f->fs_handle);
+
+    *res = fs_write(off, f->fs_inode, obj, mount);
+
+    return VFS_SUCCESS;
+}
