eed98c64d781926e23b6f70eff9b79e6eec994be
Supercip971 <supercyp971@gmail.com>
2021-10-07 17:58:16 -0400

    brutal: tyding up envelope code

--- sources/kernel/envelope.c
+++ sources/kernel/envelope.c
@@ -4,20 +4,22 @@ BrResult envelope_send(Envelope *self, BrMsg const *msg, Domain *domain)
 {
     self->msg = *msg;
 
-    if (domain != nullptr)
+    if (domain == nullptr)
     {
-        for (size_t i = 0; i < BR_MSG_ARG_COUNT; i++)
+        return BR_SUCCESS;
+    }
+
+    for (size_t i = 0; i < BR_MSG_ARG_COUNT; i++)
+    {
+        if (msg->flags & BR_MSG_HND(i))
         {
-            if (msg->flags & BR_MSG_HND(i))
+            self->objs[i] = domain_lookup(domain, msg->arg[i], BR_OBJECT_ANY);
+
+            if (!self->objs[i])
             {
-                self->objs[i] = domain_lookup(domain, msg->arg[i], BR_OBJECT_ANY);
+                envelope_deinit(self);
 
-                if (!self->objs[i])
-                {
-                    envelope_deinit(self);
-
-                    return BR_BAD_HANDLE;
-                }
+                return BR_BAD_HANDLE;
             }
         }
     }
@@ -29,14 +31,18 @@ BrResult envelope_recv(Envelope *self, BrMsg *msg, Domain *domain)
 {
     *msg = self->msg;
 
-    if (domain != nullptr)
+    if (domain == nullptr)
     {
-        for (size_t i = 0; i < BR_MSG_ARG_COUNT; i++)
+        envelope_deinit(self);
+
+        return BR_SUCCESS;
+    }
+
+    for (size_t i = 0; i < BR_MSG_ARG_COUNT; i++)
+    {
+        if (self->objs[i])
         {
-            if (self->objs[i])
-            {
-                self->msg.arg[i] = domain_add(domain, self->objs[i]);
-            }
+            self->msg.arg[i] = domain_add(domain, self->objs[i]);
         }
     }
 
