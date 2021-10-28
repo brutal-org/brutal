37adc8a0dbaa61098bc04d759b06b6bad7d3dda7
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-10-28 12:04:08 +0200

    brutal-math: added easing functions.

--- sources/libs/brutal/math.h
+++ sources/libs/brutal/math.h
@@ -2,6 +2,7 @@
 
 #include <brutal/base.h>
 /* --- */
+#include <brutal/math/easing.h>
 #include <brutal/math/edge.h>
 #include <brutal/math/rect.h>
 #include <brutal/math/trans2.h>
---
+++ sources/libs/brutal/math/easing.h
@@ -0,0 +1,65 @@
+#pragma once
+
+typedef double (*Ease)(double);
+
+double ease_linear(double p);
+
+double ease_quadratic_in(double p);
+
+double ease_quadratic_out(double p);
+
+double ease_quadratic_in_out(double p);
+
+double ease_cubic_in(double p);
+
+double ease_cubic_out(double p);
+
+double ease_cubic_in_out(double p);
+
+double ease_quartic_in(double p);
+
+double ease_quartic_out(double p);
+
+double ease_quartic_in_out(double p);
+
+double ease_quintic_in(double p);
+
+double ease_quintic_out(double p);
+
+double ease_quintic_in_out(double p);
+
+double ease_sine_in(double p);
+
+double ease_sine_out(double p);
+
+double ease_sine_in_out(double p);
+
+double ease_circular_in(double p);
+
+double ease_circular_out(double p);
+
+double ease_circular_in_out(double p);
+
+double ease_exponential_in(double p);
+
+double ease_exponential_out(double p);
+
+double ease_exponential_in_out(double p);
+
+double ease_elastic_in(double p);
+
+double ease_elastic_out(double p);
+
+double ease_elastic_in_out(double p);
+
+double ease_back_in(double p);
+
+double ease_back_out(double p);
+
+double ease_back_in_out(double p);
+
+double ease_bounce_out(double p);
+
+double ease_bounce_in(double p);
+
+double ease_bounce_in_out(double p);
