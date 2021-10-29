bbd162c2ac120ab5f41582ebe2750ea8250a545a
Supercip971 <supercyp971@gmail.com>
2021-10-28 21:30:24 -0400

    cc: added value + expr dump

--- sources/libs/cc/gen/dump.c
+++ sources/libs/cc/gen/dump.c
@@ -1,6 +1,28 @@
 #include <cc/gen/dump.h>
 
-void cdump_value(Emit *emit, CVal value);
+void cdump_value(Emit *emit, CVal value)
+{
+    emit_fmt(emit, "value:{}\n", cval_type_to_str(value.type));
+    emit_ident(emit);
+    switch (value.type)
+    {
+    case CVAL_SIGNED:
+        emit_fmt(emit, "data: {}\n", value.signed_);
+        break;
+    case CVAL_UNSIGNED:
+        emit_fmt(emit, "data: {}\n", value.unsigned_);
+        break;
+    case CVAL_FLOAT:
+        emit_fmt(emit, "data: (float non supported in fmt)\n");
+        break;
+    case CVAL_STRING:
+        emit_fmt(emit, "data \"{}\"\n", value.string_);
+        break;
+    default:
+        assert_unreachable();
+    }
+    emit_deident(emit);
+}
 
 void cdump_type(Emit *emit, CType type)
 {
@@ -87,9 +109,84 @@ void cdump_type(Emit *emit, CType type)
     emit_deident(emit);
 }
 
-void cdump_expr(Emit *emit, CExpr expr);
+void cdump_expr(Emit *emit, CExpr expr)
+{
+    emit_fmt(emit, "expr:{} \n", cexpr_type_to_str(expr.type));
+    emit_ident(emit);
 
-void cdump_stmt(Emit *emit, CStmt stmt);
+    switch (expr.type)
+    {
+    case CEXPR_EMPTY:
+    case CEXPR_SELF:
+        break;
+
+    case CEXPR_CONSTANT:
+        cdump_value(emit, expr.constant_);
+
+    case CEXPR_IDENT:
+        emit_fmt(emit, "identifier: {}\n", expr.ident_);
+
+    case CEXPR_POSTFIX:
+        emit_fmt(emit, "op: {}\n", cop_to_str(expr.postfix_.op));
+        cdump_expr(emit, *expr.postfix_.expr);
+        break;
+
+    case CEXPR_PREFIX:
+        emit_fmt(emit, "op: {}\n", cop_to_str(expr.postfix_.op));
+        cdump_expr(emit, *expr.prefix_.expr);
+        break;
+
+    case CEXPR_INFIX:
+        emit_fmt(emit, "op: {}\n", cop_to_str(expr.infix_.op));
+        cdump_expr(emit, *expr.infix_.lhs);
+        cdump_expr(emit, *expr.infix_.rhs);
+        break;
+
+    case CEXPR_CALL:
+        cdump_expr(emit, *expr.call_.expr);
+        emit_fmt(emit, "arg:\n");
+
+        emit_ident(emit);
+
+        vec_foreach(v, &expr.call_.args)
+        {
+            cdump_expr(emit, v);
+        }
+
+        emit_deident(emit);
+        break;
+
+    case CEXPR_CAST:
+        cdump_type(emit, expr.cast_.type);
+        cdump_expr(emit, *expr.cast_.expr);
+        break;
+
+    case CEXPR_TERNARY:
+        cdump_expr(emit, *expr.ternary_.expr_cond);
+        cdump_expr(emit, *expr.ternary_.expr_true);
+        cdump_expr(emit, *expr.ternary_.expr_false);
+        break;
+
+    case CEXPR_INITIALIZER:
+        vec_foreach(v, &expr.initializer_.initializer)
+        {
+            cdump_expr(emit, v);
+        }
+        break;
+
+    default:
+        assert_unreachable();
+    }
+
+    emit_deident(emit);
+}
+
+void cdump_stmt(Emit *emit, CStmt stmt)
+{
+    todo$("dump stmt");
+    UNUSED(emit);
+    UNUSED(stmt);
+}
 
 void cdump_decl(Emit *emit, CDecl decl)
 {
@@ -100,6 +197,20 @@ void cdump_decl(Emit *emit, CDecl decl)
         emit_fmt(emit, "typedef");
         emit_fmt(emit, " {}\n", decl.name);
         cdump_type(emit, decl.type_.type);
+    }
+    else if (decl.type == CDECL_VAR)
+    {
+        emit_fmt(emit, "var");
+        emit_fmt(emit, " {}\n", decl.name);
+        cdump_type(emit, decl.var_.type);
+        cdump_expr(emit, decl.var_.expr);
+    }
+    else if (decl.type == CDECL_FUNC)
+    {
+        emit_fmt(emit, "func");
+        emit_fmt(emit, " {}\n", decl.name);
+        cdump_type(emit, decl.func_.type);
+        cdump_stmt(emit, decl.func_.body);
     }
     emit_deident(emit);
 }
