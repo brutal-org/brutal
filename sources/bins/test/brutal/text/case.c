d271d6994383c90985ea05fae6159c628a64901d
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-11-09 22:45:36 +0100

    test: added more tests for change case.

--- sources/bins/test/brutal/text/case.c
+++ sources/bins/test/brutal/text/case.c
@@ -12,7 +12,16 @@ static void test_case(Case c, char const *input, char const *expected)
 TEST(change_case)
 {
     test_case(CASE_NO, "noCase", "no case");
+    test_case(CASE_NO, "no_case", "no case");
+    test_case(CASE_NO, "NO_CASE", "no case");
+    test_case(CASE_NO, "no/case", "no case");
+    test_case(CASE_NO, "no.case", "no case");
+    test_case(CASE_NO, "No Case", "no case");
+
     test_case(CASE_DEFAULT, "Default Case", "Default Case");
+    test_case(CASE_DEFAULT, "this is unchanged", "this is unchanged");
+    test_case(CASE_DEFAULT, "this.is/unchanged", "this.is/unchanged");
+
     test_case(CASE_CAMEL, "camel case", "camelCase");
     test_case(CASE_CAPITAL, "capitalCase", "Capital Case");
     test_case(CASE_CONSTANT, "hello world", "HELLO_WORLD");
@@ -27,7 +36,9 @@ TEST(change_case)
     test_case(CASE_SWAP, "Hello World", "hELLO wORLD");
     test_case(CASE_LOWER, "HELLO WORLD", "hello world");
     test_case(CASE_LOWER_FIRST, "HELLO WORLD", "hELLO WORLD");
+    test_case(CASE_UPPER, "helloWorld", "HELLOWORLD");
     test_case(CASE_UPPER, "hello world", "HELLO WORLD");
     test_case(CASE_UPPER_FIRST, "hello world", "Hello world");
     test_case(CASE_SPONGE, "hello world", "HeLlO WoRlD");
+    test_case(CASE_SPONGE, "NFTs are the future of the web", "NfTs aRe tHe fUtUrE Of tHe wEb");
 }
