2596decf2bf1412bd09d98e62e728e6488546be5
Supercip971 <supercyp971@gmail.com>
2021-11-21 11:12:16 -0400

    kernel: (riscv) added debug IoWriter now we can use log$()

--- sources/kernel/riscv64/arch.c
+++ sources/kernel/riscv64/arch.c
@@ -1,4 +1,29 @@
 #include "kernel/arch.h"
+#include "kernel/riscv64/uart.h"
+
+static bool log_initialized = false;
+static IoWriter log;
+
+static IoResult arch_debug_write(MAYBE_UNUSED void *context, uint8_t const *data, MAYBE_UNUSED size_t offset, size_t size)
+{
+    for(size_t i = 0; i < size; i++)
+    {
+        uart_putc(data[i]);
+    }
+
+    return OK(IoResult, size);
+}
+
+IoWriter *arch_debug(void)
+{
+    if (!log_initialized)
+    {
+        log.write = arch_debug_write;
+        log_initialized = true;
+    }
+
+    return &log;
+}
 
 void arch_idle(void)
 {
@@ -25,9 +50,4 @@ uint32_t arch_in(MAYBE_UNUSED uint16_t port, MAYBE_UNUSED int size)
 
 void arch_out(MAYBE_UNUSED uint16_t port, MAYBE_UNUSED int size, MAYBE_UNUSED uint32_t data)
 {
-}
-
-IoWriter *arch_debug(void)
-{
-    return nullptr;
 }
--- sources/kernel/riscv64/boot.c
+++ sources/kernel/riscv64/boot.c
@@ -1,12 +1,11 @@
 
 
 #include "kernel/riscv64/uart.h"
+#include <brutal/debug.h>
 void arch_entry_main(void)
 {
     uart_init();
-    uart_putc('h');
-    uart_putc('i');
-    uart_putc('\n');
+    log$("hello world");
     while (1)
     {
     }
