bb54abf2069b99ad01c5f7318a0c2fe90318a985
Supercip971 <supercyp971@gmail.com>
2021-11-23 18:44:13 -0400

    kernel: tyding up code

--- sources/kernel/riscv64/boot.c
+++ sources/kernel/riscv64/boot.c
@@ -1,12 +1,12 @@
 
 
 #include <brutal/debug.h>
-#include "kernel/riscv64/fdt.h"
+#include <hw/fdt/fdt.h>
+#include <kernel/arch.h>
 #include "kernel/riscv64/interrupts.h"
 #include "kernel/riscv64/uart8250.h"
 
 void arch_entry_main(uint64_t hart_id, uint64_t fdt_addr)
-
 {
     uart8250_init();
     log$("started cpu: {} with fdt: {}", hart_id, fdt_addr);
@@ -14,7 +14,10 @@ void arch_entry_main(uint64_t hart_id, uint64_t fdt_addr)
     // sifive_uart_init();
     FdtHeader *header = (FdtHeader *)fdt_addr;
     FdtBeginNode *node = fdt_root(header);
-    dump_fdt(header, node);
+
+    Emit em;
+    emit_init(&em, arch_debug());
+    dump_fdt(&em, header, node);
 
     node = get_fdt_node((node), str$("soc"));
     node = get_fdt_node((node), str$("uart@10000000"));
