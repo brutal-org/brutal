357c475b9407abedcae6b0c35aeadde4d39fe527
Supercip971 <supercyp971@gmail.com>
2021-12-21 16:00:04 -0400

    test: added test for cc preprocessor # fixed bugs - fixed scan error when not having newline at the end - fixed null directive eating newline -

---
+++ sources/pkgs/test/cc/cc.c
@@ -0,0 +1,51 @@
+#include "test/cc/cc.h"
+#include "test/test.h"
+
+bool assert_lex_eq(Lex *first, Str left)
+{
+    int idx = 0;
+
+    vec_foreach(lexeme, &first->lexemes)
+    {
+
+        if ((size_t)idx > left.len)
+        {
+            panic$("expected len eq {} != {}", idx, left.len);
+        }
+
+        Str part = str_sub(left, idx, idx + lexeme->str.len);
+        if (!str_eq(part, lexeme->str))
+        {
+            panic$("tok '{}' != '{}'", lexeme->str, part);
+        }
+
+        idx += lexeme->str.len;
+    }
+
+    if ((size_t)idx != left.len)
+    {
+        panic$("expected len eq {} != {}", left.len, idx);
+    }
+
+    return true;
+}
+
+TEST(lex_eq_test)
+{
+
+    Str from = str$("int these_are_some_tokens >= + 10 \"efeifjieij\" \n");
+
+    ctx_lex(lexer, from);
+
+    assert_truth(assert_lex_eq(&lexer, from));
+}
+
+TEST_WITH_FLAGS(lex_eq_error_test, TEST_EXPECTED_TO_FAIL)
+{
+
+    Str from = str$("° <- this should not be valid");
+
+    ctx_lex(lexer, from);
+
+    (void)lexer;
+}
