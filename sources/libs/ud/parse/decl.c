904dc9e9068440ec04b5584c67356cf81b43761f
lg <abbix@riseup.net>
2021-12-30 15:51:04 -0500

    udfore: add support for function calls (2)

---
+++ sources/libs/ud/parse/decl.c
@@ -0,0 +1,58 @@
+#include <ud/ast.h>
+#include <ud/parse/lexer.h>
+#include <ud/parse/parse.h>
+
+UdAstNode ud_parse_decl(Lex *lex, Alloc *alloc)
+{
+    UdAstNode ret = {};
+
+    ret.type = UD_NODE_STMT;
+    ret.stmt.type = UD_STMT_DECL;
+    ret.stmt.decl_.type = UD_DECL_VAR;
+
+    if (lex_expect(lex, UDLEX_LET))
+    {
+        lex_next(lex);
+
+        UdVarDecl *decl = &ret.stmt.decl_.var;
+
+        ud_parse_whitespace(lex);
+
+        if (lex_curr(lex).type == UDLEX_IDENT)
+        {
+            decl->name = lex_curr(lex).str;
+
+            lex_next(lex);
+        }
+
+        ud_parse_whitespace(lex);
+
+        if (lex_expect(lex, UDLEX_COLON))
+        {
+            ud_parse_whitespace(lex);
+
+            ud_expect(lex, UDLEX_IDENT);
+
+            decl->type.name = lex_peek(lex, -1).str;
+        }
+
+        else
+        {
+            decl->type.name = str$("inferred");
+        }
+
+        ud_parse_whitespace(lex);
+
+        ud_expect(lex, UDLEX_EQUAL);
+
+        ud_parse_whitespace(lex);
+
+        decl->value = ud_parse_expr(lex, alloc).expr;
+
+        lex_next(lex);
+
+        ud_expect(lex, UDLEX_SEMICOLON);
+    }
+
+    return ret;
+}
