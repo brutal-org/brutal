9cf05eb627f87023c94bb11a24a473200c3b6490
lg <abbix@riseup.net>
2021-12-30 12:28:58 -0500

    udfore: tidying

--- sources/libs/ud/parse/parse.h
+++ sources/libs/ud/parse/parse.h
@@ -9,3 +9,4 @@ void ud_parse_whitespace(Lex *lex);
 bool ud_parse_expect_separator(Lex *lex, LexemeType type);
 bool ud_parse_skip_separator(Lex *lex, LexemeType type);
 bool ud_parse_is_separator(Lex *lex, LexemeType type);
+bool ud_expect(Lex *lex, LexemeType type);
