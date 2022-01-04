caa8a8db1f5f64ec56788b3f5aa079e40cce1922
Supercip971 <supercyp971@gmail.com>
2022-01-04 14:25:04 -0400

    ansi: added stdio.h - just with SEEK macros this commit is really small but I don't think  having ext2 commit interferring with the libc is a great idea

--- sources/embed/file.h
+++ sources/embed/file.h
@@ -6,13 +6,11 @@ MaybeError embed_file_open(IoFile *self, Str path);
 
 MaybeError embed_file_create(IoFile *self, Str path);
 
+MaybeError embed_file_open(IoFile *self, Str path);
+
 MaybeError embed_file_close(IoFile *self);
 
 IoResult embed_file_seek(IoFile *self, IoSeek pos);
-
-#define SEEK_SET 0
-#define SEEK_CUR 1
-#define SEEK_END 2
 
 IoResult embed_file_read(IoFile *self, uint8_t *data, size_t size);
 
