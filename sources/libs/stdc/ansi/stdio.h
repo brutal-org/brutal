b83eca972b40c7bd1b7b185832e4d4fa3f3efcc7
Supercip971 <supercyp971@gmail.com>
2022-01-04 14:25:04 -0400

    ansi: added stdio.h - just with SEEK macros this commit is really small but I don't think  having ext2 commit interferring with the libc is a great idea

---
+++ sources/libs/stdc/ansi/stdio.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/* --- 7.21 - Input/Output -------------------------------------------------- */
+
+/* --- 7.21.1 - Introduction ------------------------------------------------ */
+
+#define SEEK_CUR 0
+#define SEEK_END 1
+#define SEEK_SET 2
