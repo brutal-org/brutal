1e60f220938b1752a2a90ade1b95861518ca210e
Stephan Vedder <stephan.vedder@gmail.com>
2022-01-10 21:56:39 +0100

    codec: start huffman code

---
+++ sources/libs/brutal/codec/deflate/huff.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <brutal/types.h>
+
+/*
+ * Build a table for fast decoding of symbols from a Huffman code.  As input,
+ * this function takes the codeword length of each symbol which may be used in
+ * the code.  As output, it produces a decode table for the canonical Huffman
+ * code described by the codeword lengths.  The decode table is built with the
+ * assumption that it will be indexed with "bit-reversed" codewords, where the
+ * low-order bit is the first bit of the codeword.  This format is used for all
+ * Huffman codes in DEFLATE.
+ *
+ * @decode_table
+ *	The array in which the decode table will be generated.  This array must
+ *	have sufficient length; see the definition of the ENOUGH numbers.
+ * @lens
+ *	An array which provides, for each symbol, the length of the
+ *	corresponding codeword in bits, or 0 if the symbol is unused.  This may
+ *	alias @decode_table, since nothing is written to @decode_table until all
+ *	@lens have been consumed.  All codeword lengths are assumed to be <=
+ *	@max_codeword_len but are otherwise considered untrusted.  If they do
+ *	not form a valid Huffman code, then the decode table is not built and
+ *	%false is returned.
+ * @num_syms
+ *	The number of symbols in the code, including all unused symbols.
+ * @decode_results
+ *	An array which provides, for each symbol, the actual value to store into
+ *	the decode table.  This value will be directly produced as the result of
+ *	decoding that symbol, thereby moving the indirection out of the decode
+ *	loop and into the table initialization.
+ * @table_bits
+ *	The log base-2 of the number of main table entries to use.
+ * @max_codeword_len
+ *	The maximum allowed codeword length for this Huffman code.
+ *	Must be <= DEFLATE_MAX_CODEWORD_LEN.
+ * @sorted_syms
+ *	A temporary array of length @num_syms.
+ *
+ * Returns %true if successful; %false if the codeword lengths do not form a
+ * valid Huffman code.
+ */
+static bool
+build_decode_table(uint32_t decode_table[],
+                   const uint8_t lens[],
+                   const unsigned num_syms,
+                   const uint32_t decode_results[],
+                   const unsigned table_bits,
+                   const unsigned max_codeword_len,
+                   uint16_t *sorted_syms);
--- sources/pkgs/test/brutal/codec/deflate.c
+++ sources/pkgs/test/brutal/codec/deflate.c
@@ -8,7 +8,7 @@
 TEST(deflate_uncompressed)
 {
     uint8_t out_storage[512];
-    uint8_t in_storage[] = {'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'};
+    const uint8_t in_storage[] = {'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'};
 
     DeflateCompressionContext ctx;
     deflate_init(&ctx, 0, alloc_global());
@@ -23,7 +23,24 @@ TEST(deflate_uncompressed)
 TEST(inflate_uncompressed)
 {
     uint8_t out_storage[512];
-    uint8_t in_storage[] = {1, 11, 0, 244, 255, 'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'};
+    const uint8_t in_storage[] = {1, 11, 0, 244, 255, 'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'};
+
+    size_t size = UNWRAP(deflate_decompress_data(in_storage, sizeof(in_storage), out_storage, sizeof(out_storage)));
+
+    // A single uncompressed block has 5 extra bytes
+    assert_equal(size, sizeof(in_storage) - 5);
+}
+
+TEST(inflate_huffman)
+{
+    uint8_t out_storage[512];
+    /* 256 zero bytes compressed using Huffman only (no match or distance codes) */
+    const uint8_t in_storage[] = {
+        0x05, 0xCA, 0x81, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0xFF,
+        0xD5, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x02};
 
     size_t size = UNWRAP(deflate_decompress_data(in_storage, sizeof(in_storage), out_storage, sizeof(out_storage)));
 
