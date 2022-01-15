3a4daec67a3ebdd9e4492d3facac914260cc6a0e
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2022-01-15 16:16:40 +0100

    pci: cap2str make sure id is in range.

--- sources/libs/hw/pci/capabilities.c
+++ sources/libs/hw/pci/capabilities.c
@@ -1,15 +1,29 @@
 #include <pci/capabilities.h>
 
-const char *caps[] =
-    {
-        [PCI_CAP_POWER_MANAGEMENT] = "power management",
-        [PCI_CAP_VENDOR_SPECIFIC] = "Vendor Specific",
-        [PCI_CAP_MSI] = "MSI",
-        [PCI_CAP_PCIE] = "PCIe",
-        [PCI_CAP_MSIX] = "MSIx",
-        [PCI_CAP_SATA] = "SATA",
+const char *caps[] = {
+    [PCI_CAP_POWER_MANAGEMENT] = "power management",
+    [PCI_CAP_VENDOR_SPECIFIC] = "Vendor Specific",
+    [PCI_CAP_MSI] = "MSI",
+    [PCI_CAP_PCIE] = "PCIe",
+    [PCI_CAP_MSIX] = "MSIx",
+    [PCI_CAP_SATA] = "SATA",
 };
+
 Str cap2str(uint8_t id)
 {
-    return str$(caps[id]);
+    if (array_len$(caps) < id)
+    {
+        if (caps[id] == nullptr)
+        {
+            return str$("INVALID");
+        }
+        else
+        {
+            return str$(caps[id]);
+        }
+    }
+    else
+    {
+        return str$("INVALID");
+    }
 }
