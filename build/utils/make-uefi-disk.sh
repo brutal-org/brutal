b3a9c4a0f8230220130e02aa68fbbac68d6f72bd
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2022-01-15 15:53:50 +0100

    build: revert image generation.

--- build/utils/make-uefi-disk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "making uefi disk with size: $1 sectors"
-
-EFI_DISK=./bin/loader/disk_uefi.hdd
-dd if=/dev/zero of=$EFI_DISK bs=512 count=$1
-mkfs.fat -F 32 $EFI_DISK
-
-mcopy -snv -i ./bin/loader/disk_uefi.hdd ./bin/loader/image/* ::
