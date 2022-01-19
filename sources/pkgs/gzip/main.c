4a5316091ecbbe685e5789684eabd17c22b754f2
Stephan Vedder <stephan.vedder@gmail.com>
2022-01-19 07:57:27 +0100

    pkgs: add gzip program

---
+++ sources/pkgs/gzip/main.c
@@ -0,0 +1,30 @@
+#include <brutal/alloc.h>
+#include <brutal/codec/gzip/gzip.h>
+#include <brutal/debug.h>
+#include <brutal/io.h>
+
+int main(int argc, char const *argv[])
+{
+    if (argc < 3)
+    {
+        log$("Usage: gzip [OPTION]... [INPUT] [OUTPUT]", argv[0]);
+        return -1;
+    }
+
+    if (!str_eq(str$(argv[1]), str$("-d")))
+    {
+        return -1;
+    }
+
+    IoFile source_file;
+    UNWRAP_OR_PANIC(io_file_open(&source_file, str$(argv[2])), "File not found!");
+
+    IoFile dest_file;
+    io_file_create(&dest_file, str$(argv[3]));
+    IoResult result = gzip_decompress_stream(io_file_writer(&dest_file), io_file_reader(&source_file));
+    if (!result.succ)
+    {
+        return -1;
+    }
+    return 0;
+}
