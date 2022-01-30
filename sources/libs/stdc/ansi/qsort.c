6d3efd6360b1f2536392d48ab12f4c99d7d054be
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2022-01-30 13:40:20 +0100

    stdc-ansi: fix qsort.

--- sources/libs/stdc/ansi/qsort.c
+++ sources/libs/stdc/ansi/qsort.c
@@ -9,12 +9,12 @@ void qsort(void *base, size_t nmemb, size_t size, int (*compar)(void const *, vo
 
     for (size_t i = 0; i + 1 < nmemb; i++)
     {
-        for (size_t j = i + 1; i < nmemb; i++)
+        for (size_t j = i + 1; j < nmemb; j++)
         {
             void *a = base + (i * size);
             void *b = base + (j * size);
 
-            if (compar(a, b) < 0)
+            if (compar(a, b) > 0)
             {
                 memswap(a, b, size);
             }
