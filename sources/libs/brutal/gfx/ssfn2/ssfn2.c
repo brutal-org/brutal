263357c031768e2ef8f85280ecf2f724216fcf6e
Stephan Vedder <stephan.vedder@gmail.com>
2022-01-30 21:49:04 +0100

    ssfn2: work on character mappings

--- sources/libs/brutal/gfx/ssfn2/ssfn2.c
+++ sources/libs/brutal/gfx/ssfn2/ssfn2.c
@@ -36,6 +36,34 @@ static MaybeError ssfn2_load_string(IoReader reader, char *dst)
     return SUCCESS;
 }
 
+static MaybeError ssfn2_load_mappings(IoRSeek rseek, SSFN2FontHeader *header, SSFN2CommonHeader *common_header)
+{
+    size_t char_offs = load_le(header->characters_offs);
+    size_t lig_offs = load_le(header->ligature_offs);
+    size_t size = load_le(common_header->size);
+
+    io_seek(rseek.seeker, io_seek_from_start(char_offs));
+    size_t end = lig_offs ? lig_offs : size - 4;
+
+    int unicode = 0;
+    for (size_t unicode = 0; UNWRAP(io_tell(rseek.seeker)) < end;)
+    {
+        uint8_t val;
+        TRY(MaybeError, io_read_byte(rseek.reader, &val));
+
+        if(val == 0b11111111)
+        {
+            unicode += 65356;
+        }
+        else if(val & 0b11000000)
+        {
+            
+        }
+    }
+
+    return SUCCESS;
+}
+
 static MaybeError ssfn2_load_stringtable(IoReader reader, SSFN2Font *font)
 {
     TRY(MaybeError, ssfn2_load_string(reader, font->stringtable.font_name));
@@ -47,21 +75,23 @@ static MaybeError ssfn2_load_stringtable(IoReader reader, SSFN2Font *font)
     return SUCCESS;
 }
 
-static MaybeError ssfn2_load_internal(IoReader reader, SSFN2Font *font)
+static MaybeError ssfn2_load_internal(IoRSeek rseek, SSFN2Font *font)
 {
+    IoReader reader = rseek.reader;
     SSFN2CommonHeader common_header;
     TRY(MaybeError, io_read(reader, (uint8_t *)&common_header, sizeof(SSFN2CommonHeader)));
 
     if (memcmp(common_header.magic, SSFN2_COLLECTION, 4) == 0)
     {
         // uint32_t end = load_le(common_header.size);
-        while (ssfn2_load_internal(reader, font).succ)
+        while (ssfn2_load_internal(rseek, font).succ)
             ;
     }
     else if (memcmp(common_header.magic, SSFN2_MAGIC, 4) == 0)
     {
         TRY(MaybeError, io_read(reader, (uint8_t *)&font->header, sizeof(SSFN2FontHeader)));
         TRY(MaybeError, ssfn2_load_stringtable(reader, font));
+        TRY(MaybeError, ssfn2_load_mappings(rseek, &font->header, &common_header));
     }
     else
     {
