c115e281f646b6501f7153853b232ecefe2cea34
VAN BOSSUYT Nicolas <nvanbossuyt@underside.be>
2022-02-08 16:18:54 +0100

    ssfn2: fix unchecked buffer access.

--- sources/libs/brutal/gfx/font.h
+++ sources/libs/brutal/gfx/font.h
@@ -102,5 +102,6 @@ extern uint8_t gfx_font_builtin_data[256 * 16];
 GfxFont gfx_font_builtin(void);
 
 /* --- SSFN2 Font ----------------------------------------------------------- */
+
 struct SSFN2Font;
 GfxFont gfx_font_ssfn2(struct SSFN2Font *);
