1c28ead0cd63430b9a6a7369cd23c11a1bdbe7d6
VAN BOSSUYT Nicolas <nvanbossuyt@underside.be>
2022-02-18 13:27:29 +0100

    brutal-text: utf8 stream decoder.

--- sources/libs/brutal/text/utf8.h
+++ sources/libs/brutal/text/utf8.h
@@ -14,3 +14,22 @@ size_t utf8_byte_len(uint8_t str);
 uint8_t *utf8_str_to_cstr(Str str, Alloc *alloc);
 
 bool uft8_next_rune(Str *str, Rune *rune);
+
+typedef struct
+{
+    size_t len;
+    uint8_t buffer[5];
+} Utf8Decoder;
+
+static inline bool utf8_decode(Utf8Decoder *self, uint8_t byte, Rune *rune)
+{
+    self->buffer[self->len++] = byte;
+
+    if (utf8_byte_len(self->buffer[0]) == self->len)
+    {
+        *rune = utf8_to_rune(str_n$(self->len, (const char *)self->buffer));
+        return true;
+    }
+
+    return false;
+}
