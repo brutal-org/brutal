822dbec1af33aefbe48c9cd33162a892fc7dbbcb
Supercip971 <supercyp971@gmail.com>
2022-02-26 21:16:14 +0400

    cc: added error dumping

--- sources/utils/cc/main.c
+++ sources/utils/cc/main.c
@@ -4,8 +4,176 @@
 #include <cc/lex/pproc.h>
 #include <cc/parse.h>
 #include <cc/sema.h>
+#include <cc/sema/checks.h>
 #include <cc/trans.h>
 #include <json/emit.h>
+
+typedef struct
+{
+    int line_number_start; // the number of the line at the start
+    int line_number_end;   // the number of the line at the end
+    int line_start;        // the starting character index  aligned by a line
+    int line_end;          // the last character index aligned by a line
+    int line_len;          // the length of the line(s)
+    bool is_multiline;     // if the range is represented by multiple or a single line
+} SrcFilePosInfo;
+
+// FIXME: this will be moved in an other file later.
+SrcFilePosInfo report_info(SrcRef ref, Str src)
+{
+    int begin = ref.begin;
+    int end = ref.end;
+    int line_len;
+
+    int line_number_start = str_count_chr(str_sub(src, 0, begin), '\n') + 1;
+    int line_number_end = str_count_chr(str_sub(src, 0, end), '\n') + 1;
+    int line_start = str_last_chr(str_sub(src, 0, begin), '\n') + 1;
+    int line_end = str_first_chr(str_sub(src, end, src.len), '\n') + end;
+    bool is_multiline = line_number_start != line_number_end;
+    if (!is_multiline)
+    {
+        line_len = str_first_chr(str_sub(src, line_start, src.len), '\n');
+    }
+    else
+    {
+        line_len = str_last_chr(str_sub(src, line_start, line_end + 1), '\n');
+    }
+
+    if (line_len == -1)
+    {
+        line_len = src.len;
+    }
+
+    return (SrcFilePosInfo){
+        .is_multiline = is_multiline,
+        .line_len = line_len,
+        .line_end = line_end,
+        .line_start = line_start,
+        .line_number_end = line_number_end,
+        .line_number_start = line_number_start,
+    };
+}
+
+void report_csema_line(Emit *out, Str src, SrcFilePosInfo info, bool only_first_line)
+{
+    Str line = str_sub(src, info.line_start, info.line_start + info.line_len);
+    emit_fmt(out, "{3d fg-blue} {fg-blue} ", info.line_number_start, "|");
+
+    if (info.is_multiline && !only_first_line)
+    {
+        emit_fmt(out, "{fg-red} ", "/");
+    }
+
+    int line_number = info.line_number_start;
+    for (int i = 0; i < info.line_len; i++)
+    {
+        if (line.buf[i] == '\n')
+        {
+            if (only_first_line)
+            {
+                break;
+            }
+            line_number++;
+            emit_fmt(out, "\n{3d fg-blue} {fg-blue} ", line_number, "|");
+
+            if (info.is_multiline)
+            {
+                emit_fmt(out, "{fg-red} ", "|");
+            }
+        }
+        else
+        {
+            emit_fmt(out, "{}", line.buf[i]);
+        }
+    }
+}
+
+void report_csema_dump(CSemaReport report, Emit *out, Scan *source, Str filename)
+{
+    SrcRef src_ref = report.info.cref.ref;
+    int begin = src_ref.begin;
+    int end = src_ref.end;
+
+    if (report.level == CSEMA_WARN)
+    {
+        emit_fmt(out, "\n! warn: {red}\n", report.info.msg);
+    }
+    else if (report.level == CSEMA_ERR)
+    {
+        emit_fmt(out, "\n!! {fg-red bold}({fg-red}): {bold}\n", "error", report.pass, report.info.msg);
+    }
+
+    Str src = str_n$(source->size, (char *)source->buf);
+    SrcFilePosInfo info = report_info(src_ref, src);
+    // number of line from begin
+    emit_fmt(out, "-> {fg-blue}:{}:{} \n", filename, info.line_number_start, begin - info.line_start);
+
+    report_csema_line(out, src, info, false);
+
+    if (!info.is_multiline)
+    {
+        emit_fmt(out, "\n    : ");
+
+        for (int i = info.line_start; i < info.line_len + info.line_start; i++)
+        {
+            if (i > begin && i <= end)
+            {
+                emit_fmt(out, "{fg-red}", "^");
+            }
+            else
+            {
+                emit_fmt(out, " ");
+            }
+        }
+        emit_fmt(out, " {fg-red}", report.info.msg);
+    }
+
+    vec_foreach(v, &report.comments)
+    {
+
+        bool is_comment_inline = v->cref.ref.begin >= info.line_start && v->cref.ref.end <= (info.line_len + info.line_start);
+
+        if (!info.is_multiline && is_comment_inline)
+        {
+            emit_fmt(out, "\n    : ");
+
+            for (int i = info.line_start; i < m_min(info.line_len + info.line_start, v->cref.ref.end + 2); i++)
+            {
+                if (i > v->cref.ref.begin && i <= v->cref.ref.end)
+                {
+                    emit_fmt(out, "{fg-blue}", "-");
+                }
+                else
+                {
+                    emit_fmt(out, " ");
+                }
+            }
+        }
+        else
+        {
+            emit_fmt(out, "\n");
+            SrcFilePosInfo info2 = report_info(v->cref.ref, src);
+            report_csema_line(out, src, info2, true);
+
+            emit_fmt(out, "\n    : ");
+            for (int i = info2.line_start; i < m_min(info2.line_len + info2.line_start, v->cref.ref.end + 2); i++)
+            {
+                if (i > v->cref.ref.begin && i <= v->cref.ref.end)
+                {
+                    emit_fmt(out, "{fg-blue}", "-");
+                }
+                else
+                {
+                    emit_fmt(out, " ");
+                }
+            }
+            emit_fmt(out, " ");
+        }
+        emit_fmt(out, "{fg-blue}", v->msg);
+    }
+
+    emit_fmt(out, "\n");
+}
 
 int main(int argc, char const *argv[])
 {
@@ -40,11 +208,21 @@ int main(int argc, char const *argv[])
     CUnit unit = cparse_unit(&processed, base$(&heap));
     CSema sema;
     csema_init(&sema, base$(&heap));
+
     unit = csema_unit(&sema, unit, base$(&heap));
+
+    do_sema_checks(&unit, &sema);
+
     Emit emit;
     emit_init(&emit, io_chan_out());
 
     emit_fmt$(&emit, "\n");
+    vec_foreach_v(report, &sema.reports)
+    {
+        report_csema_dump(report, &emit, &scan, str$(argv[1]));
+    }
+
+    emit_fmt(&emit, "\n");
 
     emit_fmt$(&emit, "--- BEGIN OG CODE ---\n");
     emit_fmt$(&emit, "{}\n", buf_str(&source_buf));
