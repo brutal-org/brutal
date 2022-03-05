6068eb27e02b24fd85b5d38c8479c4c40b639ccb
Supercip971 <supercyp971@gmail.com>
2022-03-05 17:05:42 +0000

    embed: added a function to get the time in nanosecond

--- sources/libs/embed/posix/time.c
+++ sources/libs/embed/posix/time.c
@@ -9,6 +9,13 @@ Tick embed_time_current_tick(void)
     return (uint64_t)(ts.tv_nsec / 1000000) + ((uint64_t)ts.tv_sec * 1000ull);
 }
 
+unsigned long embed_time_current_nsec(void)
+{
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return (ts.tv_nsec) + ((uint64_t)ts.tv_sec * 1000000000ull);
+}
+
 TimeStamp embed_time_current_timestamp(void)
 {
     return (TimeStamp)time(nullptr);
