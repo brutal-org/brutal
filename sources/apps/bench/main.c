8667f1c098693aa63e498303b8cd442e0c10a93a
Supercip971 <supercyp971@gmail.com>
2022-03-06 15:58:09 +0000

    bench: improved the application [...] - use double for delta time - added event handling - removed frame pack as they are useless when we deal with nanoseconds

--- sources/apps/bench/main.c
+++ sources/apps/bench/main.c
@@ -3,13 +3,11 @@
 #include <brutal/parse.h>
 #include <brutal/time.h>
 #include <brutal/ui.h>
-#include <embed/time.h>
 #include <embed/win.h>
 #include "bench.h"
 
 // frames pack: the number of frames to pack into a single tick count:
 // low frames pack are not really good when you do fast benchmark.
-#define FRAMES_PACK 8
 
 static size_t _len = 0;
 static Bench _benchs[1024] = {};
@@ -30,7 +28,7 @@ static void bench_run_tick(Gfx *self, Bench bench, int tick)
     bench.func(self, tick);
 }
 
-static void bench_draw_fps(Gfx *gfx, float avg_delta)
+static void bench_draw_fps(Gfx *gfx, double avg_delta)
 {
     Str v = str_fmt(alloc_global(), "fps: {} (dt: {})", 1.f / avg_delta, avg_delta);
     gfx_fill_style(gfx, gfx_paint_fill(GFX_WHITE));
@@ -38,7 +36,7 @@ static void bench_draw_fps(Gfx *gfx, float avg_delta)
     alloc_free(alloc_global(), (void *)v.buf);
 }
 
-static float bench_run(UiWin *win, Bench bench, int sec_per_bench)
+static float bench_run(UiApp *app, UiWin* win, Bench bench, int sec_per_bench)
 {
     gfx_begin(&win->gfx, ui_win_gfx(win));
     gfx_clip(&win->gfx, ui_win_bound(win));
@@ -46,23 +44,24 @@ static float bench_run(UiWin *win, Bench bench, int sec_per_bench)
     gfx_font_family(&win->gfx, win->app->font);
     gfx_push(&win->gfx);
 
+    // only used to know when we need to stop the benchmark, this hasn't any implication in the calculation of the framerate/dt
     TimeStamp time_begin = timestamp_now();
 
-    float avg_delta = 0;
+    double avg_delta = 0;
     long frame = 0;
 
-    while (time_begin + sec_per_bench > timestamp_now())
+    while (time_begin + sec_per_bench > timestamp_now() && app->alive)
     {
         gfx_clear(&win->gfx, GFX_BLACK);
 
-        unsigned long start = embed_time_current_nsec();
-        for (int j = 0; j < FRAMES_PACK; j++)
-        {
-            bench_run_tick(&win->gfx, bench, frame + j);
-        }
-        unsigned long end = embed_time_current_nsec();
+        // here we calculate the time it tooks to render the benchmark
+        NanoSeconds start = time_ns_now();
 
-        float delta_s = (end - start) / (1000000000.f * FRAMES_PACK);
+        bench_run_tick(&win->gfx, bench, frame);
+
+        NanoSeconds end = time_ns_now();
+
+        double delta_s = (end - start) / (double)(1000000000.f);
 
         // add delta_s to to the average delta time
         avg_delta = ((avg_delta * frame) + delta_s) / (frame + 1.f);
@@ -71,33 +70,36 @@ static float bench_run(UiWin *win, Bench bench, int sec_per_bench)
         embed_win_flip(win, ui_win_bound(win));
 
         frame++;
+
+        ui_app_pump(app);
+
     }
 
     gfx_pop(&win->gfx);
     gfx_end(&win->gfx);
 
-    log$("'{}': fps: {} / dt: {}s for {} frames", bench.name, 1 / (avg_delta), avg_delta, frame * FRAMES_PACK);
+    log$("'{}': fps: {} / dt: {}s for {} frames", bench.name, 1 / (avg_delta), avg_delta, frame );
     return avg_delta;
 }
 
-static int bench_run_by_pattern(Str pattern, UiWin *win, int sec_per_bench)
+static int bench_run_by_pattern(Str pattern, UiApp* app, UiWin *win, int sec_per_bench)
 {
     for (size_t i = 0; i < _len; i++)
     {
         if (glob_match_str(pattern, str$(_benchs[i].name)))
         {
-            bench_run(win, _benchs[i], sec_per_bench);
+            bench_run(app, win, _benchs[i], sec_per_bench);
         }
     }
 
     return 0;
 }
 
-static int bench_run_all(UiWin *win, int sec_count)
+static int bench_run_all(UiApp* app, UiWin *win, int sec_count)
 {
     for (size_t i = 0; i < _len; i++)
     {
-        bench_run(win, _benchs[i], sec_count);
+        bench_run(app, win, _benchs[i], sec_count);
     }
 
     return 0;
@@ -132,7 +134,6 @@ int main(int argc, char const *argv[])
             assert_lower_than(i + 1, argc);
             sec_per_bench = 0;
             str_to_int(str$(argv[i + 1]), &sec_per_bench);
-            log$("sec per bench: {} ({})", sec_per_bench, argv[i + 1]);
             i++;
         }
     }
@@ -145,11 +146,11 @@ int main(int argc, char const *argv[])
 
     if (str_any(test_to_run))
     {
-        bench_run_by_pattern(test_to_run, win, sec_per_bench);
+        bench_run_by_pattern(test_to_run, &app, win, sec_per_bench);
     }
     else
     {
-        bench_run_all(win, sec_per_bench);
+        bench_run_all(&app, win, sec_per_bench);
     }
 
     ui_win_deref(win);
