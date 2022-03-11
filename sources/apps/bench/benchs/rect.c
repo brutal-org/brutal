e5eb24862b1e2a31ff4e0b3cce73221eb29012cc
VAN BOSSUYT Nicolas <nvanbossuyt@underside.be>
2022-03-11 15:24:01 +0100

    benchs: improved text benchmark.

--- sources/apps/bench/benchs/rect.c
+++ sources/apps/bench/benchs/rect.c
@@ -45,6 +45,5 @@ BENCH(rounded_rect_fill_gradient)
     };
 
     gfx_fill_style(gfx, gfx_paint_gradient(grad));
-
     gfx_fill_rect(gfx, m_rectf(10, 10, 512, 512), 32);
 }
--- sources/apps/bench/benchs/text.c
+++ sources/apps/bench/benchs/text.c
@@ -4,5 +4,14 @@
 BENCH(basic_text)
 {
     gfx_fill_style(gfx, gfx_paint_fill(UI_COLOR_BASE08));
-    gfx_text(gfx, m_vec2f(64, 64), str$("abcdefghijklmnopqrstuvwxyz \n brutal is love brutal is live"));
+
+    float scale = 1.f;
+    float pos = 64.f;
+    for (int i = 0; i < 8; i++)
+    {
+        gfx_font_style(gfx, (GfxFontStyle){.scale = scale});
+        gfx_text(gfx, m_vec2f(18, pos), str$("The quick brown fox jumps over the lazy dog."));
+        scale *= 1.25;
+        pos += scale * 16;
+    }
 }
