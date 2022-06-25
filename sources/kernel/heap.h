58948b4418b1f34362dd51d57cb45125965e6fbf
Supercip971 <supercyp971@gmail.com>
2022-06-25 16:47:54 +0000

    kernel: added heap_alloc_lower

--- sources/kernel/heap.h
+++ sources/kernel/heap.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <bal/abi.h>
+
 #include "kernel/pmm.h"
 
 typedef USizeRange HeapRange;
@@ -8,6 +9,8 @@ typedef USizeRange HeapRange;
 typedef Result(BrResult, HeapRange) HeapResult;
 
 HeapResult heap_alloc(size_t size);
+
+HeapResult heap_alloc_lower(size_t size);
 
 HeapResult heap_free(HeapRange range);
 
--- sources/kernel/x86_64/heap.c
+++ sources/kernel/x86_64/heap.c
@@ -1,22 +1,33 @@
 #include "kernel/heap.h"
 #include <brutal-debug>
 #include <brutal-sync>
+
 #include "kernel/mmap.h"
 #include "kernel/pmm.h"
 
 static Lock _lock;
 
-HeapResult heap_alloc(size_t size)
+static HeapResult heap_alloc_impl(size_t size, bool higher)
 {
     size = align_up$(size, MEM_PAGE_SIZE);
 
     LOCK_RETAINER(&_lock)
 
-    PmmRange pmm_range = TRY(HeapResult, pmm_alloc(size, true));
+    PmmRange pmm_range = TRY(HeapResult, pmm_alloc(size, higher));
     pmm_range.base = mmap_phys_to_io(pmm_range.base);
     HeapRange heap_range = range$(HeapRange, pmm_range);
 
     return OK(HeapResult, heap_range);
+}
+
+HeapResult heap_alloc(size_t size)
+{
+    return heap_alloc_impl(size, true);
+}
+
+HeapResult heap_alloc_lower(size_t size)
+{
+    return heap_alloc_impl(size, false);
 }
 
 HeapResult heap_free(HeapRange range)
