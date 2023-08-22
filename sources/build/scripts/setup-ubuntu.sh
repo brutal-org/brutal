2253ac300560ee37d304cff735876a762505b317
Jon Daniel <jondaniel879@gmail.com>
2023-08-22 20:41:48 +0200

    update LLVM and GCC version

--- sources/build/scripts/setup-ubuntu.sh
+++ sources/build/scripts/setup-ubuntu.sh
@@ -7,15 +7,15 @@ sudo apt update
 # LLVM/CLANG
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
-sudo ./llvm.sh 13 all
+sudo ./llvm.sh 17 all
 
 # GCC/binutils
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt install -y gcc-11 g++-11 binutils
+sudo apt install -y gcc-13 g++-13 binutils
 sudo update-alternatives \
-    --install /usr/bin/gcc gcc /usr/bin/gcc-11 100 \
-    --slave /usr/bin/g++ g++ /usr/bin/g++-11 \
-    --slave /usr/bin/gcov gcov /usr/bin/gcov-11
+    --install /usr/bin/gcc gcc /usr/bin/gcc-13 100 \
+    --slave /usr/bin/g++ g++ /usr/bin/g++-13 \
+    --slave /usr/bin/gcov gcov /usr/bin/gcov-13
 
 # The rest
 sudo apt install -y make build-essential bison flex texinfo nasm mtools wget \
